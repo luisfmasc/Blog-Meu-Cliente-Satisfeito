f80215aa53b81f193385acf22d0a8b58945ec2fc
Luis Mascarenhas <contato@luisfmasc.com.br>
2019-02-17 17:44:36 -0300

    Create footer.php

---
+++ components/footer.php
@@ -0,0 +1,28 @@
+<footer class="azul">
+	<div class="container">
+		<div class="row">
+			<address class="col-sm-6 col-md-6 col-md-offset-0" id="endereco" onClick="gtag('event', 'endereco', {'event_category' : 'footer', 'event_label' : 'selecionaEndereco'})">
+				<a href="mailto:contato@meuclientesatisfeito.com">contato@meuclientesatisfeito.com.</a>
+			</address>
+		  </div>
+			<div class="col-sm-3 col-lg-offset-1 col-lg-2 col-sm-offset-0 col-md-offset-1 col-xs-offset-0 col-xs-5" id="social">
+			  <a class="btn btn-social-icon " href="https://www.facebook.com/#" target="_blank" onClick="gtag('event', 'botaoSocial', {'event_category' : 'footer', 'event_label' : 'facebook'})"  title="Siga-nos no Facebook">
+				<span class="fa fa-facebook"></span>
+			  </a>
+			  <a class="btn btn-social-icon" href="http://instagram.com/#" target="_blank" onClick="gtag('event', 'botaoSocial', {'event_category' : 'footer', 'event_label' : 'instagram'})" title="Siga-nos no Instagram">
+				<span class="fa fa-instagram"></span>
+			  </a>
+			  <a class="btn btn-social-icon" href="https://www.linkedin.com/company/#" target="_blank" onClick="gtag('event', 'botaoSocial', {'event_category' : 'footer', 'event_label' : 'linkedin'})" title="Siga-nos no Linkedin">
+				<span class="fa fa-linkedin"></span>
+			  </a>
+			</div>
+			<div class="clearfix"></div>
+			<p id="juridico">2019 Meu Cliente Satisfeito. Todos os direitos reservados. | <a href="../politicaDePrivacidade.html" target="_blank" onClick="gtag('event', 'politica', {'event_category' : 'footer', 'event_label' : 'politicas'})">Política de Privacidade</a>.</p>
+		</div>
+	</div>
+</footer>
+
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.min.css" integrity="sha256-rFMLRbqAytD9ic/37Rnzr2Ycy/RlpxE5QH52h7VoIZo=" crossorigin="anonymous" />
+	<script src="js/popper.min.js"></script> 
+	<script src="js/bootstrap-4.2.1.js"></script>
