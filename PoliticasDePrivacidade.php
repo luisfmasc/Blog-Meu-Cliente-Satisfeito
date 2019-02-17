eaed3b7cf714f859b12815757fc7232af768ba46
Luis Mascarenhas <contato@luisfmasc.com.br>
2019-02-17 17:45:21 -0300

    Create PoliticasDePrivacidade.php

---
+++ PoliticasDePrivacidade.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta property="og:url"                content="https://meuclientesatisfeito.com" />
+	<meta property="og:type"               content="blog" />
+	<meta property="og:title"              content="Meu cliente satisfeito" />
+	<meta property="og:description"        content="" />
+	<meta property="og:image"              content="https://meuclientesatisfeito.com/images/institucional.png" />
+
+    <title>Política de privacidade | Meu cliente satisfeito</title>
+	<link href="css/bootstrap-4.2.1.css" rel="stylesheet">
+	<?php  $menuAtivo = 'privacidade';
+	  include('components/analytcs.html'); ?>
+	<script>gtag('event', 'page_view', {
+		  'send_to': 'UA-110534198-4',
+		});
+	</script>		
+  </head>
+
+
+<body>
+	<header>
+		<?php include('components/header.php'); ?>
+		<?php include('components/menu.php'); ?>
+	</header>
+	
+	<main id="politica" class="container">
+		<section class="row">
+			<div class="col-lg-12">
+				<h1>Política de privacidade</h1>
+					<p>Aqui na <b>Meu Cliente Satisfeito</b> estamos comprometidos em resguardar sua privacidade. O intuito deste documento é esclarecer quais informações são coletadas dos usuários de nosso site <a href="https://meuclientesatisfeito.com">meuclientesatisfeito.com</a> e respectivos serviços – e de que forma esses dados são manipulados e utilizados.</p>
+					<p>Alertamos que se você não concorda com o conteúdo desta política, não é recomendável baixar nossos materiais nem utilizar quaisquer de nossos serviços.</p>
+				<h2>Sobre a coleta de dados</h2>
+					<p>Em nossos sites, as informações são coletadas das seguintes formas:</p>
+					<ul>
+						<li>Informações fornecidas por você – Coletamos informações de identificação pessoal (como nome, e-mail, empresa em que trabalha e cargo) via preenchimento dos formulários para download de nossos conteúdos gratuitos. Eventualmente, a solicitação algumas informações podem ser feitas por meio de contato direto com os usuários via e-mail ou telefone.</li>
+						<li>Informações de navegação no site – Quando você visita nosso site, é inserido um ‘cookie’ no seu navegador por meio do Google Analytics e do Facebook Analytics, para identificar quantas vezes você retorna ao nosso endereço. São coletadas, anonimamente, informações, como endereço IP, localização geográfica, fonte de referência, tipo de navegador, duração da visita e páginas visitadas.</li>
+					</ul>
+				<h2>Sobre o uso de suas Informações Pessoais</h2>
+					<p>O presente termo permite que a <b>Meu Cliente Satisfeito</b> use suas informações pessoais para diferentes finalidades.</p>
+					<p>O e-mail é utilizado para a operação de envio do material ou informação por você requisitada no preenchimento do formulário. Também pode ser usado para envio de Newsletters, com materiais relacionados ao curso pretendido.</p>
+					<p>Por fim, o e-mail será utilizado ainda para comunicar o lançamento de novos materiais gratuitos ou de novos produtos da <b>Meu Cliente Satisfeito</b> e parceiros. No entanto, o usuário pode cancelar a assinatura a qualquer momento.</p>
+					<p>Colaboradores da <b>Meu Cliente Satisfeito</b> poderão eventualmente entrar em contato via e-mail ou telefone para fazer pesquisas ou apresentar produtos e serviços.</p>
+					<p>Suas informações pessoais serão compartilhadas com nossos parceiros apenas quando a parceria estiver explícita na página onde consta o formulário para o recebimento de conteúdo educativo.</p>
+				<h2>Sobre o acesso às suas informações pessoais</h2>
+					<p>Poderão ver suas informações pessoais apenas funcionários da <b>Meu Cliente Satisfeito</b>. Eventualmente, caso a inserção de suas informações se dê em ações criadas em parcerias, os parceiros explicitamente identificados também terão acesso à informação. Nenhuma informação pessoal poderá ser divulgada publicamente.</p>
+					<p>A <b>Meu Cliente Satisfeito</b> também se compromete a não vender, alugar ou repassar suas informações para terceiros. A única exceção está em casos em que essas informações forem exigidas judicialmente.</p>
+					<p>Além disso, embora trabalhemos com boas práticas de proteção e segurança, nenhum serviço web possui 100% de garantia contra invasões e não podemos nos responsabilizar caso isso ocorra.</p>
+				<h2>Sobre o cancelamento da assinatura e alteração/exclusão de informações pessoais</h2>
+					<p>Você pode optar por não receber mais qualquer tipo de e-mail da <b>Meu Cliente Satisfeito</b>.</p>
+					<p>Em todos os e-mails que enviamos há sempre um link para cancelar a assinatura disponível nas últimas linhas. Ao clicar nesse link você será automaticamente descadastrado da lista.</p>
+					<p>É importante mencionar que ao preencher qualquer formulário novamente ficará caracterizada a reinserção do seu e-mail à lista. Portanto, a requisição de cancelamento deve ser feita novamente caso seja de seu interesse.</p>
+					<p>Para alterar suas informações pessoais ou mesmo excluí-las do nosso banco de dados, basta enviar um e-mail para <a href="mailto:contato@meuclientesatisfeito.com">contato@meuclientesatisfeito.com.</a></p>
+				<h2>Mudanças na Política de Privacidade</h2>
+					<p>Essa Política de Privacidade pode passar por atualizações. Desta forma, recomendamos visitar periodicamente esta página para que você tenha conhecimento sobre as modificações.</p>
+					<p>Antes de usar informações para outros fins que não os definidos nesta Política de Privacidade, solicitaremos sua autorização.</p>
+			</div>
+		</section>
+	</main>
+	<?php include('components/footer.php'); ?>
+</body>
+</html>
