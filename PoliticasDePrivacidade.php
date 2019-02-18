1d1ba51fc3dc4a0924a023c46df8f211ff0726fc
Luis Mascarenhas <contato@luisfmasc.com.br>
2019-02-18 01:59:03 -0300

    Update PoliticasDePrivacidade.php
    Corrige sync

--- PoliticasDePrivacidade.php
+++ PoliticasDePrivacidade.php
@@ -14,7 +14,7 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 	<link href="css/style.css" rel="stylesheet">
 	<?php  $menuAtivo = 'privacidade';
-	  include('components/analytcs.html'); ?>
+	  include('components/analytics.html'); ?>
 	<script>gtag('event', 'page_view', {
 		  'send_to': 'UA-110534198-4',
 		});
