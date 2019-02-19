2e4d123ab2c90bd363e8d72ed8716e4e0b6893ce
Ivan Flecha Ribeiro <ivanflecharibeiro@gmail.com>
2019-02-19 14:49:25 -0300

    Create NPS-O-que-é-e-porque-é-importante-para-sua-empresa.php
    Post da Iza usando o sublime pra facilitar nossa vida

---
+++ NPS-O-que-é-e-porque-é-importante-para-sua-empresa.php
@@ -0,0 +1,267 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta property="og:url" content="https://meuclientesatisfeito.com"/>
+	<meta property="og:type" content="post"/>
+	<meta property="og:title" content="Meu cliente satisfeito"/> <!---->
+	<meta property="og:description" content=""/> <!---->
+	<meta property="og:image" content="https://meuclientesatisfeito.com/images/institucional.png"/> <!--tumb-->
+	<title>A pergunta definitiva | Meu cliente satisfeito</title> <!---->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<link href="css/style.css" rel="stylesheet">
+	<?php  $menuAtivo = 'post';
+	  include('components/analytics.html'); ?>
+	<script>
+		gtag( 'event', 'page_view', {
+			'send_to': 'UA-134713675-1',
+		} );
+	</script>
+</head>
+<body>
+	<header>
+		<?php include('components/menu.php'); ?>
+	</header>
+
+	<main id="politica" class="container">
+		<section class="row">
+			<main class="col-md-8">
+				<h1 class="mt-5">NPS: O que é e porque é importante para sua empresa</h1> <!---->
+				<small>18 de fevereiro de 2019, Izabella Martins, NPS</small><br>
+				<figure class="figure my-5">
+					<img src="images\1_XkAbdFvgce6DzrLnS5rU6g.jpeg" class="figure-img img-fluid rounded" alt="NPS: O que é e porque é importante para sua empresa"> <!---->
+					<figcaption class="figure-caption">NPS: O que é e porque é importante para sua empresa</figcaption>
+				</figure>
+
+<br>
+<h3>Em uma escala de 0 a 10, qual a chance dos seus clientes indicarem sua empresa a um amigo ou colega?</h3>
+<p><h1><b>NPS: O que é e porque é importante para sua empresa</b></h1>
+</br>
+</br>
+<p>NPS é a sigla para</span><b> Net Promoter Score,</b><span style="font-weight: 400;"> uma metodologia para medir o </span><b>grau de satisfação dos clientes</b> <span style="font-weight: 400;">com a empresa. Em tradução direta, significa "Nota Média de Indicação" e retrata em números o quão seu cliente está satisfeito com o atendimento, produto ou serviço prestado, além do grau de lealdade e recorrência.</p>
+
+</br>
+
+<h2><b>Porque o NPS é importante para minha empresa?</b></h2>
+<span style="font-weight: 400;">Com certeza você já ouviu o bordão "O cliente sempre tem razão", certo? Bom, isto não é uma máxima, mas uma coisa é certa: com ou sem razão, o cliente pode levar sua empresa ao topo ou ao declínio. Como? É aí que entra o NPS!</span>
+
+</br>
+
+<span style="font-weight: 400;">A satisfação do cliente está diretamente ligada ao</span><a href="https://surfedigital.io/blog/ltv-como-calcular/"> <span style="font-weight: 400;">Lifetime Value</span></a><span style="font-weight: 400;"> (ou LTV: valor do tempo de vida do cliente na sua empresa). A equação é simples:</span>
+
+</br>
+
+<b>Cliente satisfeito = indicações + recompra + fidelização</b>
+
+<b>Cliente insatisfeito = detração + churn + perda de LTV</b>
+
+</br>
+
+<span style="font-weight: 400;">Medir o NPS é importante para entender o quão saudável está a relação empresa x cliente e como isso se reflete nos resultados.</span>
+
+</br>
+
+<span style="font-weight: 400;">Para te ajudar, reflita sobre as seguintes questões:</span>
+<ul>
+ 	<li><span style="font-weight: 400;">Para você, a maioria dos seus clientes está satisfeito ou insatisfeito com sua empresa?</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">Seus clientes recomendam a empresa?</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">Qual a percentagem dos clientes que voltaram a comprar com sua empresa?</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">Como você mede a eficácia entre o que a empresa promete como valores ou o que é prometido ao cliente e o que realmente está sendo entregue na ponta?</span></li>
+</ul>
+<span style="font-weight: 400;">Se você -  CEO, diretor, gestor -  não soube responder com segurança às perguntas acima, fique alerta pois pode haver um grave problema de integração do time e medição de resultados efetivos. </span>
+
+</br>
+
+<span style="font-weight: 400;">Uma dica importante é: </span><b>não espere perder clientes para fazer uma pesquisa de satisfação!</b><span style="font-weight: 400;"> A pesquisa pode ser feitas pelas seguintes razões:</span>
+
+</br>
+<table>
+<tbody>
+<tr>
+<td><b>Quando a empresa já não vai muito bem</b></td>
+<td><b>Melhoria de processos e aumento de lucro</b></td>
+</tr>
+<tr>
+<td>
+<ol>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">churn alto</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">dificuldade em atrair novos prospects</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">alto índice de reclamação dos clientes,no canal de contato ou nos órgãos de defesa do consumidor</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">branding fraco/insuficiente</span></li>
+</ol>
+</td>
+<td>
+<ol>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">aumentar LTV por cliente</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">melhorar capacitação do time</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">aumentar indicações, reduzindo custo inicial com novos prospects</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">garantir que o que foi prometido ao cliente seja entregue</span></li>
+</ol>
+</td>
+</tr>
+</tbody>
+</table>
+<h2><b>Como medir o NPS? </b></h2>
+<span style="font-weight: 400;">Para medir o NPS, é preciso pensar em uma pergunta simples, objetiva e com resultados muito significativos: </span>
+<h6><b>"Numa escala de 0 a 10, o quanto você indicaria nossa empresa para um amigo?"</b></h6>
+</br>
+
+<span style="font-weight: 400;">Esta é a pergunta do milhão! A partir daí você vai começar a conhecer sua carteira de clientes. </span>
+
+<span style="font-weight: 400;">Mas como tudo numa empresa, não é só perguntar de qualquer forma, é necessário planejamento! Para medir o NPS de forma eficaz, siga os passos:</span>
+
+</br>
+<h4><b>Passo 1: Planejamento</b></h4>
+</br>
+
+<span style="font-weight: 400;">Desenhe um fluxo de quando e como a pesquisa de satisfação será feita:</span>
+<ul>
+ 	<li style="font-weight: 400;"><b>ONDE</b><span style="font-weight: 400;"> - qual será o canal de comunicação</span>
+<ul>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">presencial</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">e-mail</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">telefone</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">whatsapp</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">SMS</span></li>
+</ul>
+</li>
+</ul>
+<ul>
+ 	<li style="font-weight: 400;"><b>QUANDO</b><span style="font-weight: 400;"> - em que momento a pesquisa será realizada</span>
+<ul>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">logo após fechar negócio</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">após o pagamento</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">após x dias/meses da primeira compra</span></li>
+</ul>
+</li>
+</ul>
+<ul>
+ 	<li style="font-weight: 400;"><b>O QUE</b><span style="font-weight: 400;"> - quais perguntas serão feitas aos clientes</span>
+<ul>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">somente a escala 0 a 10</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">exigir ou não explicação para notas baixas</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">mais perguntas sobre produto/serviço</span></li>
+</ul>
+</li>
+</ul>
+<ul>
+ 	<li style="font-weight: 400;"><b>QUEM</b><span style="font-weight: 400;"> - para quem a pesquisa será enviada</span>
+<ul>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">para toda a carteira de clientes</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">para os clientes mais recentes</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">para os mais antigos</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">para os que geraram churn</span></li>
+</ul>
+</li>
+</ul>
+<span style="font-weight: 400;">Viu, como não é "só fazer uma pergunta"? O planejar sua estratégia irá impactar diretamente no resultado da pesquisa. Para exemplificar, vou te contar um </span><b>case sobre o NPS na Disney</b><span style="font-weight: 400;">! </span>
+
+</br>
+
+<span style="font-weight: 400;">Você sabe quanto tempo após a visita a Disney geralmente envia a pesquisa para seus clientes? </span>
+
+<span style="font-weight: 400;">Um mês! Isso tem uma boa explicação: um mês após a visita é geralmente o tempo que leva para a fatura do cartão de crédito chegar. Logo, a empresa quer medir a </span><b>real satisfação do cliente</b><span style="font-weight: 400;">, saber se o investimento valeu a pena. </span>
+
+</br>
+
+<span style="font-weight: 400;">E aí já coloco mais uma reflexão: se até a Disney, que reune o maior acervo de mídia e entretenimento do pla-ne-ta(!), </span><b>se preocupa com NPS</b><span style="font-weight: 400;">, o que sua empresa está esperando???</span>
+
+</br>
+<h4><b>Passo 2: Execução</b></h4>
+</br>
+
+<span style="font-weight: 400;">Hora de colocar a mão na massa! Após organizar o processo, comece a pesquisa de satisfação. Não se atenha a demandas de TI: o envio pode começar manual, se preciso (pense no</span><a href="https://endeavor.org.br/estrategia-e-gestao/mvp/"> <span style="font-weight: 400;">MVP</span></a><span style="font-weight: 400;">) e depois passar para automação.</span>
+
+</br>
+
+<span style="font-weight: 400;">É importante que as mensagens de envio e de respostas sejam padronizadas e que o e-mail contenha a marca da empresa, para facilitar o reconhecimento por parte do cliente. </span>
+
+</br>
+
+<span style="font-weight: 400;">Certifique-se de que todas as respostas estejam sendo coletadas e armazenadas corretamente. Faça muitos testes antes dos envios!</span>
+
+</br>
+
+<span style="font-weight: 400;">Caso não queira desenvolver uma plataforma própria para pesquisa, tem ótimas empresas no mercado especializadas nesta área:</span><a href="https://www.opinionbox.com/"> <span style="font-weight: 400;">Opinion Box</span></a><span style="font-weight: 400;">,</span><a href="https://opinia.me/"> <span style="font-weight: 400;">Opinia</span></a> <span style="font-weight: 400;">e a </span><a href="https://tracksale.co/"><span style="font-weight: 400;">Tracksale. </span></a>
+
+</br>
+<h4><b>Passo 3: Apuração de dados</b></h4>
+</br>
+
+<span style="font-weight: 400;">Após coletar os dados, você precisa entendê-los. Seus clientes serão separados em três níveis: </span><b>Promotor, Neutro e Detrator. </b><span style="font-weight: 400;">Esta divisão se dará a partir da satisfação, da seguinte forma:</span>
+
+</br>
+<h6><b>Clientes Promotores: notas 9 a 10</b></h6>
+</br>
+
+<span style="font-weight: 400;">Estes são os clientes felizes. A possibilidade de se tornarem clientes fiéis e indicarem a empresa é muito grande. Oferecem feedbacks positivos e aumentam o faturamento da empresa. O desafio aqui é manter estes clientes felizes, promovendo ações personalizadas, que o façam sentir especial.</span>
+<h6><b>Clientes Neutros: notas 7 a 8</b></h6>
+</br>
+
+<span style="font-weight: 400;">Só buscam a empresa quando precisam, portanto, não são clientes leais. Podem fazer comprar de forma aleatória, mas com foco no produto/benefício, e não na empresa. Este tipo de cliente geralmente gosta do produto, mas tem ressalvas. Por exemplo: "O produto é bom, mas demorou a chegar" ou "o preço é o melhor, mas não gostei do atendimento". Neste caso precisa-se entender o ponto negativo listado pelo cliente e melhorá-lo, pois este pode ser um futuro "Promotor".</span>
+<h6><b>Clientes Detratores: notas 0 a 6</b></h6>
+</br>
+
+<span style="font-weight: 400;">Estes são os insatisfeitos. São eles que irão falar mal da sua empresa, abrir reclamações, devolver o produto e têm chance de nunca mais voltar. Se este índice está alto, você tem um grande problema. É preciso identificar quais as principais reclamações dos clientes e atuar para que os erros não voltem a acontecer. Para isso todas as áreas devem trabalhar em conjunto e o foco deve ser a resolução do problema, e não "quem causou o problema". </span>
+
+</br>
+<h4><b>Passo 4: Resultados</b></h4>
+</br>
+
+<span style="font-weight: 400;">É hora de fazer uma conta final: qual é seu NPS? Medir o NPS é muito simples: </span>
+
+</br>
+
+<b>NPS = % Promotores (-) % Detratores</b>
+
+</br>
+
+<span style="font-weight: 400;">Ou seja, se sua empresa teve 60% de promotores e 15% de detratores, o NPS é de 45%.  Isso significa que seus clientes têm 45% de chance de indicar sua empresa.</span>
+
+</br>
+
+<span style="font-weight: 400;">Para saber se isso é bom ou não, veja as métricas:</span>
+<h6><b>Pontuação entre 75 e 100: Excelente</b></h6>
+</br>
+
+<span style="font-weight: 400;">Sua empresa consegue entregar para o cliente exatamente ou mais do que foi prometido, passando uma percepção muito positiva.</span>
+<h6><b>Pontuação entre 50 e 74: Qualidade</b></h6>
+</br>
+
+<span style="font-weight: 400;">Sua empresa acerta em muitos aspectos na percepção do cliente, mas deixou pontos a desejar. Aqui é a zona do "mas". É boa nisso mas não é boa naquilo. </span>
+<h6><b>Pontuação entre 0 e 49: Muito a melhorar</b></h6>
+</br>
+
+<span style="font-weight: 400;">Tem muitos pontos importantes de ajustes e a percepção que está sendo passada para o consumidor não é boa. É bom revisar todo o processo, valores e alinhamento do time, para corrigir os problemas o quanto antes.</span>
+<h6><b>Pontuação negativa: Ruim</b></h6>
+</br>
+
+<span style="font-weight: 400;">Se o resultado do percentual de promotores menos o percentual de detratores deu negativo, aperte o sinal de alerta e reúna as peças-chave do time para reavaliar o MPV da entrega ao cliente. Aqui, nem o mínimo prometido está sendo entregue. Entenda as lacunas e corrija-as imediatamente. Repense também a estratégia: a empresa está no ramo certo, fazendo a coisa certa para o público coerente? </span>
+<h3><b>Conclusão</b></h3>
+</br>
+
+<span style="font-weight: 400;">Agora você já sabe que</span><b> medir a satisfação dos seus clientes te faz ganhar dinheiro</b><span style="font-weight: 400;">! Um cliente Promotor (feliz):</span>
+<ol>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">indica da sua empresa para outros prospects, o que poupa investimentos em mídia e captação de leads</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">reforça sua marca no mercado, ajudando com o branding para que sua empresa se torne referência no que faz</span></li>
+ 	<li style="font-weight: 400;"><span style="font-weight: 400;">tem maior probabilidade de voltar a negociar com a empresa</span></li>
+</ol>
+<b>Fique ligado:</b><span style="font-weight: 400;"> reforço da marca não necessariamente está ligado ao consumo direto. Se você perguntar para alguém que não gosta de refrigerante "Qual a primeira marca de refrigerante que lhe vem à cabeça", esta pessoa vai responder que é a Coca Cola, mesma que não tenha costume de consumir. Utilizando de outro bordão: "Quem não é visto não é lembrado"!</span>
+
+
+</p>
+
+				<ul class="tagsFooter list-inline my-3">
+					<li class="list-inline-item"><a href="#">Tag1</a></li> <!---->
+				</ul>
+			</main>
+			<aside class="col-md-3 offset-md-1">
+				<?php include('components/sidebar.php'); ?>
+			</aside>
+		</section>
+	</main>
+	<?php include('components/footer.php'); ?>
+</body>
+</html>
