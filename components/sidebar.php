5b5022fa42f553ec8e6b5cebbcb480bae4f3da02
Luis Mascarenhas <contato@luisfmasc.com.br>
2019-02-19 16:01:46 -0300

    Update sidebar.php
    O arquivo tava muito zuado, agora está com o conteudo quase pronto. Falta referenciar as imagens.

--- components/sidebar.php
+++ components/sidebar.php
@@ -13,90 +13,41 @@
 </section>
 <section id="tagsSidebar" class="my-5">
 	<h4>Tags</h4>
-	<ul class="tags_footer list-inline">
-		<li class="list-inline-item"><a href="#">Tag1</a>
-		</li>
-		<li class="list-inline-item"><a href="#">Tag2</a>
-		</li>
-		<li class="list-inline-item"><a href="#">Tag3</a>
-		</li>
-		<li class="list-inline-item"><a href="#">Tag4</a>
-		</li>
-		<li class="list-inline-item"><a href="#">Tag5</a>
-		</li>
-		<li class="list-inline-item"><a href="#">Tag6</a>
-		</li>
-		<li class="list-inline-item"><a href="#">Tag7</a>
-		</li>
-		<li class="list-inline-item"><a href="#">Tag8</a>
-		</li>
-		<li class="list-inline-item"><a href="#">Tag9</a>
-		</li>
+	<ul class="numesTags list-inline">
+		<li class="list-inline-item"><a href="#">Como funciona NPS</a></li>
+		<li class="list-inline-item"><a href="#">Como medir satisfação dos clientes</a></li>
+		<li class="list-inline-item"><a href="#">Experiência do cliente</a></li>
+		<li class="list-inline-item"><a href="#">Importância do NPS</a></li>
+		<li class="list-inline-item"><a href="#">NPS</a></li>
+		<li class="list-inline-item"><a href="#">NPS no Brasil
+		<li class="list-inline-item"><a href="#">o que é NPS</a></li>
+		<li class="list-inline-item"><a href="#">Pesquisa de opinião</a></li>
+		<li class="list-inline-item"><a href="#">Promotores da marca</a></li>
+		<li class="list-inline-item"><a href="#">Satisfação de clientes</a></li>
+		<li class="list-inline-item"><a href="#">Sucesso da marca</a></li>
+		<li class="list-inline-item"><a href="#">Sucesso do cliente</a></li>
 	</ul>
 </section>
 <section id="maisLidas" class="my-5">
 	<h4>Mais Lidas</h4>
 	<ul class="maisLidas list-unstyled">
 		<li class="media">
-			<svg class="bd-placeholder-img mr-3" width="100" height="100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder:100x100">
-				<rect fill="#868e96" width="100%" height="100%"></rect><text fill="#dee2e6" dy="0.15em" x="20%" y="50%">100x100</text>
-			</svg>
+			<img src="../images/thumbs/nps-o-que-e-thumb.jpg" class=" mr-3" width="100" height="100" a href="../nps-o-que-e.php">
 			<div class="media-body">
-				<h5><a href="muita-treta.php">Muita treta</a></h5>
+				<h5><a href="../nps-o-que-e.php">NPS: O que é e porque é importante para sua empresa</a></h5>
 			</div>
 		</li>
 		<li class="media my-3">
-			<svg class="bd-placeholder-img mr-3" width="100" height="100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder:120x100">
-				<rect fill="#868e96" width="100%" height="100%"></rect><text fill="#dee2e6" dy="0.15em" x="20%" y="50%">100x100</text>
-			</svg>
+			<img src="../images/thumbs/o-basico-de-nps-thumb.jpg" class=" mr-3" width="100" height="100" a href="../o-basico-de-nps.php">
 			<div class="media-body">
-				<h5><a href="#">Título do Post</a></h5>
+				<h5><a href="../o-basico-de-nps">O Básico de NPS Que Você Precisa Saber</a></h5>
 			</div>
-			<button type="submit" class="btn btn-primary">Cadastrar</button>
-		</form>
-	</section>
-	<section id="tagsSidebar" class="my-5">
-		<h4>Tags</h4>
-		<ul class="tags_footer list-inline">
-			<li class="list-inline-item"><a href="#">Tag1</a></li>
-			<li class="list-inline-item"><a href="#">Tag2</a></li>
-			<li class="list-inline-item"><a href="#">Tag3</a></li>
-			<li class="list-inline-item"><a href="#">Tag4</a></li>
-			<li class="list-inline-item"><a href="#">Tag5</a></li>
-			<li class="list-inline-item"><a href="#">Tag6</a></li>
-			<li class="list-inline-item"><a href="#">Tag7</a></li>
-			<li class="list-inline-item"><a href="#">Tag8</a></li>
-			<li class="list-inline-item"><a href="#">Tag9</a></li>
-		</ul>
-	</section>
-	<section id="maisLidas" class="my-5">
-		<h4>Mais Lidas</h4>
-		<ul class="maisLidas list-unstyled">
-			<li class="media">
-				<svg class="bd-placeholder-img mr-3" width="100" height="100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder:100x100">
-					<rect fill="#868e96" width="100%" height="100%"></rect><text fill="#dee2e6" dy="0.15em" x="20%" y="50%">100x100</text>
-				</svg>
-				<div class="media-body">
-					<h5><a href="#">Título do Post</a></h5>
-				</div>
-			</li>
-			<li class="media my-3">
-				<svg class="bd-placeholder-img mr-3" width="100" height="100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder:120x100">
-					<rect fill="#868e96" width="100%" height="100%"></rect><text fill="#dee2e6" dy="0.15em" x="20%" y="50%">100x100</text>
-				</svg>
-				<div class="media-body">
-					<h5><a href="#">Título do Post</a></h5>
-				</div>
-			</li>
-			<li class="media my-3">
-				<svg class="bd-placeholder-img mr-3" width="100" height="100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder:100x100">
-					<rect fill="#868e96" width="100%" height="100%"></rect><text fill="#dee2e6" dy="0.15em" x="20%" y="50%">100x100</text>
-				</svg>
-				<div class="media-body">
-					<h5><a href="#">Título do Post</a></h5>
-				</div>
-			</li>
-		</ul>
-
-
-	</section>
+		</li>
+		<li class="media my-3">
+			<img src="../images/thumbs/nps-nas-empresas-brasileiras-thumb.jpg" class=" mr-3" width="100" height="100" a href="../nps-nas-empresas-brasileiras.php">
+			<div class="media-body">
+				<h5><a href="../nps-nas-empresas-brasileiras.php">5 Motivos para praticar o NPS nas empresas brasileiras</a></h5>
+			</div>
+		</li>
+	</ul>
+</section>
