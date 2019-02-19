8e5259a8425dba6dc3391cd2441621168447792f
Ivan Flecha Ribeiro <ivanflecharibeiro@gmail.com>
2019-02-19 11:04:59 -0300

    Desfiz o cmmit

--- obasicodenps.php
+++ obasicodenps.php
@@ -9,7 +9,7 @@
 	<meta property="og:title" content="Meu cliente satisfeito"/> <!---->
 	<meta property="og:description" content=""/> <!---->
 	<meta property="og:image" content="https://meuclientesatisfeito.com/images/institucional.png"/> <!--tumb-->
-	<title>O Tásico de NPS| Meu cliente satisfeito</title> <!---->
+	<title>O Básico de NPS| Meu cliente satisfeito</title> <!---->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 	<link href="../css/style.css" rel="stylesheet">
 	<?php  $menuAtivo = 'post';
@@ -25,7 +25,7 @@
 		<?php include('menu.php'); ?>
 	</header>
 
-	<main id="politica" class="container">
+	<main id="post" class="container">
 		<section class="row">
 			<main class="col-md-8">
 				<h1 class="mt-5">O Básico de NPS Que Você Precisa Saber</h1> <!---->
