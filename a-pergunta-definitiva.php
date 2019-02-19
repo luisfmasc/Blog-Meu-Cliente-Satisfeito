aeee271b90ae753711f9e7c8598ceb2170db8c2c
Ivan Flecha Ribeiro <ivanflecharibeiro@gmail.com>
2019-02-19 14:12:24 -0300

    Criação de post
    Criando post de número '?' (ver planilha https://docs.google.com/spreadsheets/d/1wS-3gpSkAP0GoF9LfhYKM2XD-FyuUGdIqHi9M6Dsmkk/edit#gid=0)

---
+++ a-pergunta-definitiva.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta property="og:url" content="https://meuclientesatisfeito.com"/>
+	<meta property="og:type" content="post"/>
+	<meta property="og:title" content="Meu cliente satisfeito"/> <!---->
+	<meta property="og:description" content=""/> <!---->
+	<meta property="og:image" content="https://meuclientesatisfeito.com/images/institucional.png"/> <!--tumb-->
+	<title>A pergunta definitiva | Meu cliente satisfeito</title> <!---->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<link href="css/style.css" rel="stylesheet">
+	<?php  $menuAtivo = 'post';
+	  include('components/analytics.html'); ?>
+	<script>
+		gtag( 'event', 'page_view', {
+			'send_to': 'UA-134713675-1',
+		} );
+	</script>
+</head>
+<body>
+	<header>
+		<?php include('components/menu.php'); ?>
+	</header>
+
+	<main id="politica" class="container">
+		<section class="row">
+			<main class="col-md-8">
+				<h1 class="mt-5">A pergunta definitiva</h1> <!---->
+				<small>18 de fevereiro de 2019, Ivan Flecha, Gestão de experiência do cliente</small><br>
+				<figure class="figure my-5">
+					<img src="images\1_XkAbdFvgce6DzrLnS5rU6g.jpeg" class="figure-img img-fluid rounded" alt="Em uma escala de 0 a 10, qual a chance dos seus clientes indicarem sua empresa a um amigo ou colega?"> <!---->
+					<figcaption class="figure-caption">Em uma escala de 0 a 10, qual a chance dos seus clientes indicarem sua empresa a um amigo ou colega??</figcaption>
+				</figure>
+
+<br>
+<h3>Em uma escala de 0 a 10, qual a chance dos seus clientes indicarem sua empresa a um amigo ou colega?</h3>
+<p>Se você não sabe a resposta desta pergunta, seu negócio está em risco!
+
+Os clientes detratores — insatisfeitos com seu produto/serviço estão à solta falando mal da sua empresa. Para que seu negócio continue a crescer de forma saudável é preciso acabar com a ameaça que eles representam à sua marca.
+
+‘Como eu lido com esse tipo de prejuízo?’, você me pergunta, a resposta são quatro passos que eu irei te ensinar neste post.
+<br>
+<figure class="figure my-5">
+<img src="\images\image1.jpg" class="figure-img img-fluid rounded" alt="Logos de empresas usando NPS">
+</figure>
+<br><br>
+<h3>2. O NPS é importante para o sucesso da sua empresa!</h3>
+Diversas empresas utilizam esta metodologia para medir a satisfação de seus clientes e verificar em quais área é necessário um plano de ação. O que Allianz Group, Canon, Ebay, Facebook, HP, Microsoft e General Motors tem em comum? Todas utilizam esta metodologia em suas empresas!
+
+O NPS, hoje é a principal métrica focada na fidelidade e experiência do cliente no mundo, ou seja, impacta diretamente no resultado de uma marca e na percepção do cliente ao adquire produtos e serviços dessa marca. Influenciando ainda na recomendação de fato, ou seja, fazem com que os consumidores recomendem a marca a amigos e familiares.
+
+A aplicação NPS está tão difundida, que atualmente 95% das 500 maiores empresas no mundo utilizam essa métrica de lealdade do cliente como termômetro de relacionamento com os mesmos.
+<h3>3. Sua empresa deve utilizar o NPS!</h3>
+
+A Bain Company produziu um estudo focado em NPS de instituições financeiras no Brasil e mostrou que apesar do crescimento orgânico em suas receitas, os bancos de média renda possuem um baixo NPS, ou seja, maior volume de clientes detratores. Os bancos de alta renda foram as instituições que apresentaram maior NPS em relação aos de média renda. Nestes casos, quanto maior o número de clientes promotores, maior a aceitação de produtos e maior a permanência dos clientes com a empresa. Uma vigilância contínua desta métrica possibilita uma revisão constante dos pontos que atraem ou que repelem os clientes, gerando assim oportunidades de melhorar estes serviços para conquistar um número maior de promotores, concomitantemente aumento na retenção e receitas das empresas.
+<h3>4. As vantagens do NPS.</h3>
+
+Vamos avaliar os aspectos positivos do sistema NPS e ver como sua empresa se beneficia se os resultados forem aplicados às suas práticas de negócios.
+<ul>
+<li>Intuitivo e fácil de usar</li>
+A pergunta do modelo de questionário se baseia em uma ideia: descobrir se os seus clientes gostam o bastante da sua empresa a ponto de recomendá-la. Esta pergunta costuma ser acompanhada por uma pergunta para descobrir porque as pessoas recomendam, ou não. Você pode facilmente enviar o questionário para os clientes por e-mail, whatsapp ou até publicá-lo no seu site. O cálculo é intuitivo e exige apenas uma planilha básica para funcionar.
+
+
+<li>Excepcional para gestão</li>
+Quando os executivos da sua empresa precisam de uma medida prática, rápida e abrangente da fidelidade dos seus clientes, o NPS é uma ótima solução. Além de ajudar a empresa a comparar seu desempenho ao da concorrência e pode ser usado pelos gestores para comparar seus diversos produtos ou serviços.
+
+
+<li>Padroniza a classificação da clientela</li>
+A metodologia NPS se subdivide em três categorias de clientes: Promotores, Detratores e Passivos. Estas categorias facilitam a classificação da fidelidade dos clientes e padroniza a comunicação e os esforços da sua empresa na hora de tratar sobre a clientela.
+
+
+<li>Maior facilidade para se referenciar</li>
+Um recurso essencial pois a medida é usada como padrão por empresas no mundo todo, facilitando a comparação do seu Net Promoter Score com os seus principais concorrentes. Sua pontuação se torna mais significativa em contexto com outras pontuações do seu setor.
+</ul>
+
+<h3>5. Não deixe o NPS para manhã sua empresa precisar dele!</h3>
+Seja qual for o segmento, nada que superasse o NPS foi inventado até os tempos atuais. Uma métrica que traz insights gerais sobre a experiência do cliente, trazendo uma métrica da lealdade que reflete no crescimento do faturamento das empresas.
+
+Embora o NPS tenha seus fãs e críticos, a coisa mais importante a lembrar é que os clientes têm mais poder do que nunca para expressar suas opiniões, que podem criar ou danificar sua marca.
+
+O mais importante é lembrar que o NPS irá gerar dados muito objetivos e de fácil comparação, perfeitos para acompanhamento e por isso, deve ser utilizado continuamente.
+
+Para encerrar, o NPS é um ótimo ponto de partida justamente por sua simplicidade. Após a análise dos resultados, o trabalho real é agir sobre os feedbacks coletados, para começar a melhorar sua organização e desenvolver as ações necessárias para aumentar o NPS e permite que você avalie perfeitamente em qual etapa do processo de crescimento a sua organização está.
+
+<br><br>
+<p>Que tal começar o ano medindo o NPS na sua empresa? Clique aqui e baixe agora sua planilha para calcular o NPS da sua empresa.
+<br><br>
+
+Não sabe medir o NPS na sua empresa? Nós podemos resolver este problema, clique aqui e solicite um atendimento especializado.
+<br><br>
+
+Não sabe medir o NPS na sua empresa? Com esse E-book vamos te ensinar a fazer isso.</p>
+
+				<ul class="tagsFooter list-inline my-3">
+					<li class="list-inline-item"><a href="#">Tag1</a></li> <!---->
+				</ul>
+			</main>
+			<aside class="col-md-3 offset-md-1">
+				<?php include('components/sidebar.php'); ?>
+			</aside>
+		</section>
+	</main>
+	<?php include('components/footer.php'); ?>
+</body>
+</html>
