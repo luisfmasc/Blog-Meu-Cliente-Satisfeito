36de9d751ef74df9bdf18c288677037f3284ef7c
Luis Mascarenhas <contato@luisfmasc.com.br>
2019-02-18 23:12:59 -0300

    Update sigle.php
    Prepara para copy

--- sigle.php
+++ sigle.php
@@ -5,14 +5,14 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta property="og:url" content="https://meuclientesatisfeito.com"/>
-	<meta property="og:type" content="blog"/>
-	<meta property="og:title" content="Meu cliente satisfeito"/>
-	<meta property="og:description" content=""/>
-	<meta property="og:image" content="https://meuclientesatisfeito.com/images/institucional.png"/>
-	<title>Titulo do post | Meu cliente satisfeito</title>
+	<meta property="og:type" content="post"/>
+	<meta property="og:title" content="Meu cliente satisfeito"/> <!---->
+	<meta property="og:description" content=""/> <!---->
+	<meta property="og:image" content="https://meuclientesatisfeito.com/images/institucional.png"/> <!--tumb-->
+	<title>Titulo do post | Meu cliente satisfeito</title> <!---->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 	<link href="css/style.css" rel="stylesheet">
-	<?php  $menuAtivo = 'privacidade';
+	<?php  $menuAtivo = 'post';
 	  include('components/analytics.html'); ?>
 	<script>
 		gtag( 'event', 'page_view', {
@@ -28,27 +28,15 @@
 	<main id="politica" class="container">
 		<section class="row">
 			<main class="col-md-8">
-				<h1 class="mt-5">Titulo do post</h1>
-				<small>Data do post, Autor, Categoria/tag</small>
+				<h1 class="mt-5">Titulo do post</h1> <!---->
+				<small>18 de fevereiro de 2019, Autor, Categoria</small> <!---->
 				<figure class="figure my-5">
-					<img src="images/bridge-2060820_1280.jpg" class="figure-img img-fluid rounded" alt="A caption for the above image.">
-					<figcaption class="figure-caption">A caption for the above image.</figcaption>
+					<img src="images/bridge-2060820_1280.jpg" class="figure-img img-fluid rounded" alt="A caption for the above image."> <!---->
+					<figcaption class="figure-caption">A caption for the above image.</figcaption> <!---->
 				</figure>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dictum elit turpis, ac mattis urna ullamcorper ut. Ut euismod diam non orci laoreet tristique. Donec sodales, enim sed mollis feugiat, diam nulla aliquet turpis, ac ullamcorper nisl felis eu nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque mattis, elit et sodales hendrerit, mi risus faucibus magna, vitae pretium mi leo vitae sem. Proin tristique et risus ut maximus. Nam et eros dignissim lorem cursus pellentesque. Quisque tortor nisi, ornare vitae venenatis ac, fringilla et magna.</p>
-				<p>Nullam tempus risus ut quam dictum, consectetur ultricies eros lobortis. Integer sed tempus mauris, eget egestas velit. Maecenas in arcu purus. Nullam sapien mi, vehicula ut pellentesque at, luctus in nibh. Donec elementum odio elit. Vestibulum vel tellus quis velit dignissim consectetur. Suspendisse id dictum enim. Donec placerat nisi risus, sit amet posuere massa vehicula eu. Curabitur eleifend aliquam imperdiet. Pellentesque finibus vestibulum arcu, ac malesuada nibh mattis ac. Curabitur pretium ultricies venenatis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent ornare congue lacus, ac eleifend eros accumsan in.</p>
-				<p>Duis hendrerit leo eget facilisis sagittis. Proin aliquam eros et tellus molestie tempor. Nulla vitae sollicitudin est, vel molestie diam. Phasellus malesuada viverra dolor, vel porttitor nisl tempor at. Phasellus finibus quam eget nulla vehicula, non ullamcorper orci posuere. Quisque sit amet leo nulla. Aenean sed egestas risus. Praesent facilisis suscipit ligula vel varius. Nam vitae lacus sit amet nulla vehicula pulvinar. Curabitur in eros sed est mattis imperdiet a laoreet ipsum. Fusce magna lacus, rutrum eget aliquet at, luctus eu massa. In aliquam nibh ipsum, ut tincidunt diam ornare vitae.</p>
-				<p>Vivamus tempus tempus ex sit amet fermentum. Praesent id libero et orci viverra pellentesque. Curabitur vestibulum purus non diam vehicula laoreet. Aliquam luctus commodo lectus, non tempus risus ultricies et. Morbi quis metus ut tellus blandit consequat. In volutpat tortor sed porta dignissim. Sed tristique nisi a arcu porta porttitor.</p>
-				<p>Nunc aliquam sem eu magna sagittis posuere. Maecenas sit amet dolor volutpat velit luctus auctor at eu leo. Sed eu neque dignissim, ultricies turpis et, pretium tellus. Donec rutrum justo ut tincidunt imperdiet. Phasellus convallis at mi euismod blandit. Morbi quam ipsum, euismod quis lorem eu, auctor facilisis leo. Nam porta, mauris a consequat facilisis, orci ipsum hendrerit risus, eget finibus nunc metus et dolor.</p>
+				<p>Lorem ipsum dolor sit amet, consect</p> <!---->
 				<ul class="tagsFooter list-inline my-3">
-					<li class="list-inline-item"><a href="#">Tag1</a></li>
-					<li class="list-inline-item"><a href="#">Tag2</a></li>
-					<li class="list-inline-item"><a href="#">Tag3</a></li>
-					<li class="list-inline-item"><a href="#">Tag4</a></li>
-					<li class="list-inline-item"><a href="#">Tag5</a></li>
-					<li class="list-inline-item"><a href="#">Tag6</a></li>
-					<li class="list-inline-item"><a href="#">Tag7</a></li>
-					<li class="list-inline-item"><a href="#">Tag8</a></li>
-					<li class="list-inline-item"><a href="#">Tag9</a></li>
+					<li class="list-inline-item"><a href="#">Tag1</a></li> <!---->
 				</ul>
 			</main>
 			<aside class="col-md-3 offset-md-1">
