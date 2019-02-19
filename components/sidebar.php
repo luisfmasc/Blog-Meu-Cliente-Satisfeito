a8d08c414361f6ac0236793074bf8cd4126859f4
Luis Mascarenhas <contato@luisfmasc.com.br>
2019-02-19 17:43:12 -0300

    Define links
    E ja coloca a tag do analytcs neles

--- components/sidebar.php
+++ components/sidebar.php
@@ -8,45 +8,45 @@
 		<div class="form-group">
 			<input type="email" class="form-control" id="Email1" aria-describedby="emailHelp" placeholder="E-mail" required>
 		</div>
-		<button type="submit" class="btn btn-primary">Cadastrar</button>
+		<button type="submit" class="btn btn-primary" onClick="gtag('event', 'Formulário', {'event_category' : 'aside', event_label' : 'Cadastrou'})">Cadastrar</button>
 	</form>
 </section>
 <section id="tagsSidebar" class="my-5">
 	<h4>Tags</h4>
 	<ul class="numesTags list-inline">
-		<li class="list-inline-item"><a href="#">Como funciona NPS</a></li>
-		<li class="list-inline-item"><a href="#">Como medir satisfação dos clientes</a></li>
-		<li class="list-inline-item"><a href="#">Experiência do cliente</a></li>
-		<li class="list-inline-item"><a href="#">Importância do NPS</a></li>
-		<li class="list-inline-item"><a href="#">NPS</a></li>
-		<li class="list-inline-item"><a href="#">NPS no Brasil
-		<li class="list-inline-item"><a href="#">o que é NPS</a></li>
-		<li class="list-inline-item"><a href="#">Pesquisa de opinião</a></li>
-		<li class="list-inline-item"><a href="#">Promotores da marca</a></li>
-		<li class="list-inline-item"><a href="#">Satisfação de clientes</a></li>
-		<li class="list-inline-item"><a href="#">Sucesso da marca</a></li>
-		<li class="list-inline-item"><a href="#">Sucesso do cliente</a></li>
+		<li class="list-inline-item"><a href="#" onClick="gtag('event', 'numesTags', {'event_category' : 'aside', event_label' : 'Como funciona NPS'})">Como funciona NPS</a></li>
+		<li class="list-inline-item"><a href="#" onClick="gtag('event', 'numesTags', {'event_category' : 'aside', event_label' : 'Como medir satisfação dos clientes'})">Como medir satisfação dos clientes</a></li>
+		<li class="list-inline-item"><a href="#" onClick="gtag('event', 'numesTags', {'event_category' : 'aside', event_label' : 'Experiência do cliente'})">Experiência do cliente</a></li>
+		<li class="list-inline-item"><a href="#" onClick="gtag('event', 'numesTags', {'event_category' : 'aside', event_label' : 'Importância do NPS'})">Importância do NPS</a></li>
+		<li class="list-inline-item"><a href="#" onClick="gtag('event', 'numesTags', {'event_category' : 'aside', event_label' : 'NPS'})">NPS</a></li>
+		<li class="list-inline-item"><a href="#" onClick="gtag('event', 'numesTags', {'event_category' : 'aside', event_label' : 'NPS no Brasil'})">NPS no Brasil</a></li>
+		<li class="list-inline-item"><a href="#" onClick="gtag('event', 'numesTags', {'event_category' : 'aside', event_label' : 'O que é NPS'})">O que é NPS</a></li>
+		<li class="list-inline-item"><a href="#" onClick="gtag('event', 'numesTags', {'event_category' : 'aside', event_label' : 'Pesquisa de opinião'})">Pesquisa de opinião</a></li>
+		<li class="list-inline-item"><a href="#" onClick="gtag('event', 'numesTags', {'event_category' : 'aside', event_label' : 'Promotores da marca'})">Promotores da marca</a></li>
+		<li class="list-inline-item"><a href="#" onClick="gtag('event', 'numesTags', {'event_category' : 'aside', event_label' : 'Satisfação de clientes'})">>Satisfação de clientes</a></li>
+		<li class="list-inline-item"><a href="#" onClick="gtag('event', 'numesTags', {'event_category' : 'aside', event_label' : 'Sucesso da marca'})">Sucesso da marca</a></li>
+		<li class="list-inline-item"><a href="#" onClick="gtag('event', 'numesTags', {'event_category' : 'aside', event_label' : 'Sucesso do cliente'})">Sucesso do cliente</a></li>
 	</ul>
 </section>
 <section id="maisLidas" class="my-5">
 	<h4>Mais Lidas</h4>
 	<ul class="maisLidas list-unstyled">
 		<li class="media">
-			<img src="../images/thumbs/nps-o-que-e-100x100.jpg" class=" mr-3" width="100" height="100" a href="../nps-o-que-e.php">
+			<img src="../images/thumbs/nps-o-que-e-100x100.jpg" class=" mr-3" width="100" height="100" a href="../nps-o-que-e.php" onClick="gtag('event', 'maisLidas', {'event_category' : 'aside', event_label' : 'artigoImagem1'})">
 			<div class="media-body">
-				<h5><a href="../nps-o-que-e.php">NPS: O que é e porque é importante para sua empresa</a></h5>
+				<h5><a href="../nps-o-que-e.php" onClick="gtag('event', 'maisLidas', {'event_category' : 'aside', event_label' : 'artigoTitulo1'})">NPS: O que é e porque é importante para sua empresa</a></h5>
 			</div>
 		</li>
 		<li class="media my-3">
-			<img src="../images/thumbs/o-basico-de-nps-100x100.jpg" class=" mr-3" width="100" height="100" a href="../o-basico-de-nps.php">
+			<img src="../images/thumbs/o-basico-de-nps-100x100.jpg" class=" mr-3" width="100" height="100" a href="../o-basico-de-nps.php" onClick="gtag('event', 'maisLidas', {'event_category' : 'aside', event_label' : 'artigoImagem2'})">
 			<div class="media-body">
-				<h5><a href="../o-basico-de-nps">O Básico de NPS Que Você Precisa Saber</a></h5>
+				<h5><a href="../o-basico-de-nps.php" onClick="gtag('event', 'maisLidas', {'event_category' : 'aside', event_label' : 'artigoTitulo2'})">O Básico de NPS Que Você Precisa Saber</a></h5>
 			</div>
 		</li>
 		<li class="media my-3">
-			<img src="../images/thumbs/nps-nas-empresas-brasileiras-100x100.jpg" class=" mr-3" width="100" height="100" a href="../nps-nas-empresas-brasileiras.php">
+			<img src="../images/thumbs/nps-nas-empresas-brasileiras-100x100.jpg" class=" mr-3" width="100" height="100" a href="../nps-nas-empresas-brasileiras.php" onClick="gtag('event', 'maisLidas', {'event_category' : 'aside', event_label' : 'artigoImagem3'})">
 			<div class="media-body">
-				<h5><a href="../nps-nas-empresas-brasileiras.php">5 Motivos para praticar o NPS nas empresas brasileiras</a></h5>
+				<h5><a href="../nps-nas-empresas-brasileiras.php" onClick="gtag('event', 'maisLidas', {'event_category' : 'aside', event_label' : 'artigoTitulo3'})">5 Motivos para praticar o NPS nas empresas brasileiras</a></h5>
 			</div>
 		</li>
 	</ul>
