3027796870e973b43e9e384a32d5b3ab098f3c6e
Ivan Flecha Ribeiro <ivanflecharibeiro@gmail.com>
2019-02-19 01:40:11 -0300

    Testando mais dois posts com metodologias diferentes

---
+++ components/obasicodenps.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta property="og:url" content="https://meuclientesatisfeito.com"/>
+	<meta property="og:type" content="post"/>
+	<meta property="og:title" content="Meu cliente satisfeito"/> <!---->
+	<meta property="og:description" content=""/> <!---->
+	<meta property="og:image" content="https://meuclientesatisfeito.com/images/institucional.png"/> <!--tumb-->
+	<title>Titulo do post | Meu cliente satisfeito</title> <!---->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<link href="../css/style.css" rel="stylesheet">
+	<?php  $menuAtivo = 'post';
+	  include('analytics.html'); ?>
+	<script>
+		gtag( 'event', 'page_view', {
+			'send_to': 'UA-134713675-1',
+		} );
+	</script>
+</head>
+<body>
+	<header>
+		<?php include('menu.php'); ?>
+	</header>
+
+	<main id="politica" class="container">
+		<section class="row">
+			<main class="col-md-8">
+				<h1 class="mt-5">O Básico de NPS Que Você Precisa Saber</h1> <!---->
+				<small>18 de fevereiro de 2019, Thamires Rocha, NPS</small> <!---->
+				<figure class="figure my-5">
+					<img src="../images/bridge-2060820_1280.jpg" class="figure-img img-fluid rounded" alt="A caption for the above image."> <!---->
+					<figcaption class="figure-caption">A caption for the above image.</figcaption> <!---->
+				</figure>
+				<p>Lorem ipsum dolor sit amet, consect</p> <!---->
+				<h1 class="mt-5">O básico de NPS que você precisa saber </h1>
+				<small>Data do post, Thamires Rocha, Categoria/tag</small>
+				<figure class="figure my-5">
+					<img src="images/bridge-2060820_1280.jpg" class="figure-img img-fluid rounded" alt="A caption for the above image.">
+					<figcaption class="figure-caption">A caption for the above image.</figcaption>
+				</figure>
+				<p>Ainda não conhece a metodologia do NPS? Então saiba qual é o método utilizado por corporações que são referência no mundo inteiro.</p>
+				<p>A metodologia do Net Promoter Scores (NPS) foi criada por Fred Reichheld, nos EUA, com intuito de medir o índice de lealdade dos clientes por meio de uma única pergunta:
+“Qual a probabilidade de você recomendar [ empresa X] a um amigo ou colega?”.</p>
+				<p>Em uma escala de 0 a 10 é possível mensurar o  grau de satisfação dos seus clientes, com base em três tipos de perfis:<br />
+Notas <b>de 0 a 06 – Clientes Detratores</b> - São aqueles clientes insatisfeitos, que não voltariam a fazer negócios com a sua empresa e que a criticam em público.<br />
+Notas <b>de 7 e 8 – Clientes Neutros</b> - Aqueles que consomem o necessário  e não são leais, nem promovem a marca.<br />
+Notas <b>de 9 e 10 – Clientes Promotores</b> - São aqueles que tiveram uma experiência feliz com a empresa, são engajados e dispõe, como o próprio nome diz, a promover a marca.</p>
+				<p>Este é um método simples, flexível e confiável para medir a satisfação dos seus clientes por isso tão utilizado por organizações dos mais variados ramos para entender os pontos de melhoria nos processos e produtos das suas empresas.</p>
+				<p>E aí, pensando na realidade da sua empresa hoje, sua base de clientes possuem mais Promotores ou Detratores? Não sabe responder e deseja saber como aplicar o NPS na sua empresa? Faça o download do nosso e-book para saber mais.</p>
+
+
+				<ul class="tagsFooter list-inline my-3">
+					<li class="list-inline-item"><a href="#">Tag1</a></li> <!---->
+				</ul>
+			</main>
+			<aside class="col-md-3 offset-md-1">
+				<?php include('sidebar.php'); ?>
+			</aside>
+		</section>
+	</main>
+	<?php include('footer.php'); ?>
+</body>
+</html>
