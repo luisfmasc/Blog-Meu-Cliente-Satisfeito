69127051f36068e1539fd325c9bafa1fb9e78c3e
Luis Mascarenhas <contato@luisfmasc.com.br>
2019-02-20 02:00:17 -0300

    Update nps-o-que-e.php
    Fechou o post

--- nps-o-que-e.php
+++ nps-o-que-e.php
@@ -28,12 +28,142 @@
 	<main id="post" class="container">
 		<section class="row">
 			<main class="col-md-8">
-				<h1 class="mt-5">NPS: O que é e porque é importante para sua empresa</h1> <!---->
+				<h1 class="mt-5">NPS: O que é e porque é importante para sua empresa</h1>
 				<small>18 de fevereiro de 2019, Izabella Martins, NPS</small><br>
 				<figure class="figure my-5">
-					<img src="images\1_XkAbdFvgce6DzrLnS5rU6g.jpeg" class="figure-img img-fluid rounded" alt="NPS: O que é e porque é importante para sua empresa"> <!---->
-					<figcaption class="figure-caption">NPS: O que é e porque é importante para sua empresa</figcaption>
+					<img src="images/post/nps-o-que-e-800x600.jpg" class="figure-img img-fluid rounded" alt="[Fotografia] Quatro pessoas sorriem segurando balões de fala">
+					<figcaption class="figure-caption">#PraCegoVer: [Fotografia] Quatro pessoas sorriem - sequência composta por uma mulher, um homem e duas mulheres, vestidos de maneira formal - segurando balões de fala feitos de papel que possuem formas e cores diferentes - balão de fala retangular e amarelo, balão de fala oval e verde, balão de fala retangular com bordas arredondadas, azul e balão de fala de papel azul turqueza.</figcaption>
 				</figure>
+				<p>NPS é a sigla para <b>Net Promoter Score</b>, uma metodologia para medir o <b>grau de satisfação dos clientes</b> com a empresa. Em tradução direta, significa "Nota Média de Indicação" e retrata em números o quão seu cliente está satisfeito com o atendimento, produto ou serviço prestado, além do grau de lealdade e recorrência.</p>
+				<h2>Porque o NPS é importante para minha empresa?</h2>
+				<p>Com certeza você já ouviu o bordão "O cliente sempre tem razão", certo? Bom, isto não é uma máxima, mas uma coisa é certa: com ou sem razão, o cliente pode levar sua empresa ao topo ou ao declínio. Como? É aí que entra o NPS!</p>
+				<p>A satisfação do cliente está diretamente ligada ao <a href="https://surfedigital.io/blog/ltv-como-calcular/"> Lifetime Value</a> (ou LTV: valor do tempo de vida do cliente na sua empresa). A equação é simples:</p>
+				<p><b>Cliente satisfeito = indicações + recompra + fidelização</b>.</p>
+				<p><b>Cliente insatisfeito = detração + churn + perda de LTV</b>.</p>
+				<p>Medir o NPS é importante para entender o quão saudável está a relação empresa x cliente e como isso se reflete nos resultados.</p>
+				<p>Para te ajudar, reflita sobre as seguintes questões:</p>
+				<ul>
+					<li>Para você, a maioria dos seus clientes está satisfeito ou insatisfeito com sua empresa?</li>
+					<li>Seus clientes recomendam a empresa?</li>
+					<li>Qual a percentagem dos clientes que voltaram a comprar com sua empresa?</li>
+					<li>Como você mede a eficácia entre o que a empresa promete como valores ou o que é prometido ao cliente e o que realmente está sendo entregue na ponta?</li>
+				</ul>
+				<p>Se você - CEO, diretor, gestor - não soube responder com segurança às perguntas acima, fique alerta pois pode haver um grave problema de integração do time e medição de resultados efetivos.</p>
+				<p>Uma dica importante é: <b>não espere perder clientes para fazer uma pesquisa de satisfação</b>! A pesquisa pode ser feitas pelas seguintes razões:</p>
+				<table class="table">
+					<thead class="thead-light">
+						<tr>
+							<th scope="col">Quando a empresa já não vai muito bem</th>
+							<th scope="col">Melhoria de processos e aumento de lucro</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Churn alto.</td>
+							<td>Aumentar LTV por cliente.</td>
+						</tr>
+						<tr>
+							<td>Dificuldade em atrair novos prospects.</td>
+							<td>Melhorar capacitação do time.</td>
+						</tr>
+						<tr>
+							<td>Alto índice de reclamação dos clientes,no canal de contato ou nos órgãos de defesa do consumidor.</td>
+							<td>Aumentar indicações, reduzindo custo inicial com novos prospects.</td>
+						</tr>
+						<tr>
+							<td>Branding fraco/insuficiente.</td>
+							<td>Garantir que o que foi prometido ao cliente seja entregue.</td>
+						</tr>
+					</tbody>
+				</table>
+				<h2>Como medir o NPS?</h2>
+				<p>Para medir o NPS, é preciso pensar em uma pergunta simples, objetiva e com resultados muito significativos: </p>
+				<h6>"Numa escala de 0 a 10, o quanto você indicaria nossa empresa para um amigo?"</h6>
+				<p>Esta é a pergunta do milhão! A partir daí você vai começar a conhecer sua carteira de clientes. Mas como tudo numa empresa, não é só perguntar de qualquer forma, é necessário planejamento! Para medir o NPS de forma eficaz, siga os passos:</p>
+				<h4>Passo 1: Planejamento</h4>
+				<p>Desenhe um fluxo de quando e como a pesquisa de satisfação será feita:</p>
+				<ul>
+					<li><b>ONDE</b> - qual será o canal de comunicação</p>
+						<ul>
+							<li>Presencial</li>
+							<li>E-mail</li>
+							<li>Telefone</li>
+							<li>WhatsApp</li>
+							<li>SMS</li>
+						</ul>
+					</li>
+				</ul>
+				<ul>
+					<li><b>QUANDO</b> - em que momento a pesquisa será realizada</p>
+						<ul>
+							<li>Logo após fechar negócio</li>
+							<li>Após o pagamento</li>
+							<li>Após x dias/meses da primeira compra</li>
+						</ul>
+					</li>
+				</ul>
+				<ul>
+					<li><b>O QUE</b> - quais perguntas serão feitas aos clientes</p>
+						<ul>
+							<li>Somente a escala 0 a 10</li>
+							<li>Exigir ou não explicação para notas baixas</li>
+							<li>Mais perguntas sobre produto/serviço</li>
+						</ul>
+					</li>
+				</ul>
+				<ul>
+					<li><b>QUEM</b> - para quem a pesquisa será enviada</p>
+						<ul>
+							<li>Para toda a carteira de clientes</li>
+							<li>Para os clientes mais recentes</li>
+							<li>Para os mais antigos</li>
+							<li>Para os que geraram churn</li>
+						</ul>
+					</li>
+				</ul>
+				<p>Viu, como não é "só fazer uma pergunta"? O planejar sua estratégia irá impactar diretamente no resultado da pesquisa. Para exemplificar, vou te contar um <b>case sobre o NPS na Disney</b>!</p>
+				<p>Você sabe quanto tempo após a visita a Disney geralmente envia a pesquisa para seus clientes?</p>
+				<p>Um mês! Isso tem uma boa explicação: um mês após a visita é geralmente o tempo que leva para a fatura do cartão de crédito chegar. Logo, a empresa quer medir a <b>real satisfação do cliente</b>, saber se o investimento valeu a pena. </p>
+				<p>E aí já coloco mais uma reflexão: se até a Disney, que reúne o maior acervo de mídia e entretenimento do pla-ne-ta(!), <b>se preocupa com NPS</b>, o que sua empresa está esperando???</p>
+
+				<h4>Passo 2: Execução</h4>
+				<p>Hora de colocar a mão na massa! Após organizar o processo, comece a pesquisa de satisfação. Não se atenha a demandas de TI: o envio pode começar manual, se preciso (pense no <a href="https://endeavor.org.br/estrategia-e-gestao/mvp">MVP</a>) e depois passar para automação.</p>
+				<p>É importante que as mensagens de envio e de respostas sejam padronizadas e que o e-mail contenha a marca da empresa, para facilitar o reconhecimento por parte do cliente.</p>
+				<p>Certifique-se de que todas as respostas estejam sendo coletadas e armazenadas corretamente. Faça muitos testes antes dos envios!</p>
+				<p>Caso não queira desenvolver uma plataforma própria para pesquisa, tem ótimas empresas no mercado especializadas nesta área: <a href="https://www.opinionbox.com/">Opinion Box</a>, <a href="https://opinia.me">Opinia</a> e a <a href="https://tracksale.co">Tracksale.</a>
+				</p>
+				<h4>Passo 3: Apuração de dados</h4>
+				<p>Após coletar os dados, você precisa entendê-los. Seus clientes serão separados em três níveis: <b>Promotor, Neutro e Detrator. </b>Esta divisão se dará a partir da satisfação, da seguinte forma:</p>
+				<h5>Clientes Promotores: notas 9 a 10</h5>
+				<p>Estes são os clientes felizes. A possibilidade de se tornarem clientes fiéis e indicarem a empresa é muito grande. Oferecem feedbacks positivos e aumentam o faturamento da empresa. O desafio aqui é manter estes clientes felizes, promovendo ações personalizadas, que o façam sentir especial.</p>
+				<h5>Clientes Neutros: notas 7 a 8</h5>
+				<p>Só buscam a empresa quando precisam, portanto, não são clientes leais. Podem fazer comprar de forma aleatória, mas com foco no produto/benefício, e não na empresa. Este tipo de cliente geralmente gosta do produto, mas tem ressalvas. Por exemplo: "O produto é bom, mas demorou a chegar" ou "o preço é o melhor, mas não gostei do atendimento". Neste caso precisa-se entender o ponto negativo listado pelo cliente e melhorá-lo, pois este pode ser um futuro "Promotor".</p>
+				<h5>Clientes Detratores: notas 0 a 6</h5>
+				<p>Estes são os insatisfeitos. São eles que irão falar mal da sua empresa, abrir reclamações, devolver o produto e têm chance de nunca mais voltar. Se este índice está alto, você tem um grande problema. É preciso identificar quais as principais reclamações dos clientes e atuar para que os erros não voltem a acontecer. Para isso todas as áreas devem trabalhar em conjunto e o foco deve ser a resolução do problema, e não "quem causou o problema". </p>
+				<h4><b>Passo 4: Resultados</b></h4>
+				<p>É hora de fazer uma conta final: qual é seu NPS? Medir o NPS é muito simples:
+				</p> < b > NPS = % Promotores( - ) % Detratores < /b>
+					<p>Ou seja, se sua empresa teve 60% de promotores e 15% de detratores, o NPS é de 45%. Isso significa que seus clientes têm 45% de chance de indicar sua empresa.</p>
+				<p>Para saber se isso é bom ou não, veja as métricas:</p>
+				<h5>Pontuação entre 75 e 100: Excelente</h5>
+				<p>Sua empresa consegue entregar para o cliente exatamente ou mais do que foi prometido, passando uma percepção muito positiva.</p>
+				<h5>Pontuação entre 50 e 74: Qualidade</h5>
+				<p>Sua empresa acerta em muitos aspectos na percepção do cliente, mas deixou pontos a desejar. Aqui é a zona do "mas". É boa nisso mas não é boa naquilo. </p>
+				<h5>Pontuação entre 0 e 49: Muito a melhorar</h5>
+				<p>Tem muitos pontos importantes de ajustes e a percepção que está sendo passada para o consumidor não é boa. É bom revisar todo o processo, valores e alinhamento do time, para corrigir os problemas o quanto antes.</p>
+				<h5>Pontuação negativa: Ruim</h5>
+				<p>Se o resultado do percentual de promotores menos o percentual de detratores deu negativo, aperte o sinal de alerta e reúna as peças-chave do time para reavaliar o MPV da entrega ao cliente. Aqui, nem o mínimo prometido está sendo entregue. Entenda as lacunas e corrija-as imediatamente. Repense também a estratégia: a empresa está no ramo certo, fazendo a coisa certa para o público coerente? </p>
+				<h3>Conclusão</h3>
+				<p>Agora você já sabe que<b> medir a satisfação dos seus clientes te faz ganhar dinheiro</b>!</p>
+				<p>Um cliente Promotor (feliz):</p>
+				<ol>
+					<li>Indica da sua empresa para outros prospects, o que poupa investimentos em mídia e captação de leads</li>
+					<li>Reforça sua marca no mercado, ajudando com o branding para que sua empresa se torne referência no que faz</li>
+					<li>Tem maior probabilidade de voltar a negociar com a empresa</li>
+				</ol>
+				<p><b>Fique ligado:</b> reforço da marca não necessariamente está ligado ao consumo direto. Se você perguntar para alguém que não gosta de refrigerante "Qual a primeira marca de refrigerante que lhe vem à cabeça", esta pessoa vai responder que é a Coca Cola, mesma que não tenha costume de consumir. Utilizando de outro bordão: "Quem não é visto não é lembrado"!</p>
+				<p>Quer se certificar de que não esquecerá de nada? Testes os conhecimentos sobre o que acabou de ler e aplique o NPS em sua empresa!</p>
+
 				<ul class="tagsFooter list-inline my-3">
 					<li class="list-inline-item"><a href="#">NPS</a></li>
 					<li class="list-inline-item"><a href="#">O que é NPS</a></li>
