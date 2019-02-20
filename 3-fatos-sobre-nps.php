e78eb7e32620f6ae185a77c5fae94510827b839c
Luis Mascarenhas <contato@luisfmasc.com.br>
2019-02-19 23:24:55 -0300

    3 fatos sobre NPS que você precisa saber
    Post Completão para revisão

--- 3-fatos-sobre-nps.php
+++ 3-fatos-sobre-nps.php
@@ -6,11 +6,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta property="og:url" content="https://meuclientesatisfeito.com/o-basico-de-nps"/>
 	<meta property="og:type" content="post"/>
-	<meta property="og:title" content="O Básico de NPS | Meu cliente satisfeito"/>
-	<meta property="og:description" content="Descubra o que é NPS e como fazer sua empresa decolar"/>
+	<meta property="og:title" content="Já sabe o que é NPS? Veja as 3 coisas que você precisa saber agora!  | Meu Cliente Satisfeito"/>
+	<meta property="og:description" content="Conheça #3 fatos sobre NPS que te ajudarão a entender melhor esta tecnologia. Como surgiu, como medir e com qual frequência pode ser aplicado."/>
 	<meta property="og:image" content="https://meuclientesatisfeito.com/images/post/o-basico-de-nps-800x600.jpg"/>
-	<meta property="description" content="Descubra o que é NPS e como fazer sua empresa decolar"/>
-	<title>O Básico de NPS | Meu cliente satisfeito</title>
+	<meta property="description" content="Conheça #3 fatos sobre NPS que te ajudarão a entender melhor esta tecnologia. Como surgiu, como medir e com qual frequência pode ser aplicado."/>
+	<title>3 fatos sobre NPS que você precisa saber | Meu cliente satisfeito</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 	<link href="../css/style.css" rel="stylesheet">
 	<?php  $menuAtivo = 'post';
@@ -29,25 +29,47 @@
 	<main class="container">
 		<section class="row">
 			<main class="col-md-8">
-				<h1 class="mt-5">O Básico de NPS Que Você Precisa Saber</h1>
+				<h1 class="mt-5">3 fatos sobre NPS que você precisa saber</h1>
 				<small class="text-left">18 de fevereiro de 2019, Thamires Rocha, NPS</small>
 				<figure class="figure my-5">
-					<img src="images/post/o-basico-de-nps-800x600.jpg" class="figure-img img-fluid rounded" alt="A caption for the above image.">
-					<figcaption class="figure-caption">A caption for the above image.</figcaption>
+					<img src="images/post/o-basico-de-nps-800x600.jpg" class="figure-img img-fluid rounded" alt="[Fotografia] Pessoas caminham entre si em um local corporativo.">
+					<figcaption class="figure-caption">[Fotografia] Pessoas caminham entre si em um local corporativo. Em cima da cabeça de cada pessoa existe um balão de pensamento em cores que variam entre vermelho, cinza e verde. Abaixo da imagem estão mais três balões compondo o termo "NPS": primeiro balão, vermelho sinalizado com a letra "N", segundo balão, cinza, sinalizado com a letra "P", terceiro balão, verde, sinalizado com a letra "S".</figcaption>
 				</figure>
 					<section id="post">
-						<p>Ainda não conhece a metodologia do NPS? Então saiba qual é o método utilizado por corporações que são referência no mundo inteiro.</p>
-						<p>A metodologia do Net Promoter Scores (NPS) foi criada por Fred Reichheld, nos EUA, com intuito de medir o índice de lealdade dos clientes por meio de uma única pergunta: “Qual a probabilidade de você recomendar [ empresa X] a um amigo ou colega?”.</p>
-						<p>Em uma escala de 0 a 10 é possível mensurar o grau de satisfação dos seus clientes, com base em três tipos de perfis:</p>
+						<p>Ainda não conhece a <b>metodologia NPS</b>? Então saiba o que as grandes empresas como Amazon, Starbucks, e Nike estão usando para melhorar seus resultados.</p>
+						<p>NPS, ou <b>Net Promoter Score</b>, hoje é é usado por<b> 95% das 500 maiores empresas do mundo</b>, segundo a revista Fortune. <a href="http://meuclientesatisfeito.com/nps-o-que-e" target="_blank" rel="noopener">Neste outro artigo</a> já falamos mais sobre o que é e como funciona o NPS. </p>
+						<p>Para melhorar ainda mais seu conhecimento sobre o assunto, listamos <b>3 fatos sobre NPS que você precisa saber:</b></p>
+						<h2>Fato #1: como surgiu o NPS</h2>
+						<p>A metodologia do Net Promoter Score (NPS) foi criada por <b>Fred Reichheld</b>, nos EUA, com intuito de medir o índice de lealdade dos clientes por meio de uma única pergunta “<b>A pergunta definitiva</b>” expressão que também é título de dois livros do autor que são a base da metodologia, lançados após a primeira apresentação sobre o tema em 2003.</p>
+						<p> Você deve estar se perguntando: "Que pergunta milagrosa é esta?". A gente te responde:</p>
+						<blockquote class="bgAzulEscuro">
+							<p><b>“Qual a probabilidade de você recomendar [empresa X] a um amigo ou colega?”</b></p>
+						</blockquote>
+						<p>Esta pergunta é facilmente respondida uma <b>escala de 0 a 10,</b> onde é possível mensurar o grau de satisfação dos seus clientes independentemente do seu tipo de negócio, com base em três tipos de perfis:</p>
 						<ul>
-							<li>Notas <b>de 0 a 06 – Clientes Detratores</b> - São aqueles clientes insatisfeitos, que não voltariam a fazer negócios com a sua empresa e que a criticam em público.</li>
+							<li>Notas <b class="Vermelho">de 0 a 06 – Clientes Detratores</b> - São aqueles clientes insatisfeitos, que não voltariam a fazer negócios com a sua empresa e que a criticam em público.</li>
 							<li>Notas <b>de 7 e 8 – Clientes Neutros</b> - Aqueles que consomem o necessário e não são leais, nem promovem a marca.</li>
-							<li>Notas <b>de 9 e 10 – Clientes Promotores</b> - São aqueles que tiveram uma experiência feliz com a empresa, são engajados e dispõe, como o próprio nome diz, a promover a marca.</li>
+							<li>Notas <b class="verde">de 9 e 10 – Clientes Promotores </b> - São aqueles que tiveram uma experiência feliz com a empresa, são engajados e tendem, como o próprio nome diz, a promover a marca.</p></li>
 						</ul>
-
-						<p>Este é um método simples, flexível e confiável para medir a satisfação dos seus clientes por isso tão utilizado por organizações dos mais variados ramos para entender os pontos de melhoria nos processos e produtos das suas empresas.</p>
-						<p>E aí, pensando na realidade da sua empresa hoje, sua base de clientes possuem mais Promotores ou Detratores? Não sabe responder e deseja saber como aplicar o NPS na sua empresa? Faça o download do nosso e-book para saber mais.</p>
-					</section>
+						<h2>Fato #2: as vantagens de se usar o NPS</h2>
+						<p>Aqui vai o top 3 de vantagens:</p>
+						<ol>
+							<li><b>Fácil entendimento pelos clientes</b>
+							</li>
+							<p> Este é um método que possui fácil assimilação por parte dos clientes, operadores e gestores. Como a pergunta principal é curta e objetiva, o cliente rapidamente entende, responde e sua empresa já começa a medir os resultados online. Caso queira fazer mais perguntas durante a pesquisa de satisfação, certifique-se de que todas estejam claras, objetivas e que tenham real necessidade para realizar melhorias na empresa.</p>
+							<li><b>Agilidade na implementação de melhorias</b>
+							</li>
+							<p> O foco do NPS é melhorar o desempenho da empresa com base nos feedbacks recebidos, e, assim que tiver um volume de amostra significativo, já pode iniciar o planejamento de melhorias e dividir as tarefas para o time.<b> Tão importante quanto realizar a pesquisa de satisfação é atuar!</b> Não basta coletar dados e não fazer nada com eles.</p>
+							<li><b>Benchmark</b>
+							</li>
+							<p> O NPS é um método muito utilizado por grandes empresas, inclusive pelas mesmas do seu segmento de mercado. Com conhecimento da concorrência e networking, é possível comparar o seu resultado com outras empresas e entender o quão bem (ou mal) você está. Mas fique atento: ninguém está ruim o suficiente que não tenha solução e nem bem o bastante que não possa melhorar!</p>
+						</ol>
+						<h2>Fato #3: com qual frequência o NPS deve ser aplicado</h2>
+						<p> Isso deve ser definido de acordo com a estratégia de cada empresa, mas o ideal é que seja de 90 em 90 dias, para que não gere incômodo ao seu cliente e para que ele consiga perceber e avaliar as melhorias que foram aplicadas desde a última pesquisa.</p>
+						<p>Você pode também dar feedback aos clientes, listando as melhorias que foram realizadas nos últimos meses. Além de gerar fidelização, faz com que seu time nunca se acomode e busque aperfeiçoamento contínuo!</p>
+						<p>E aí, pensando na realidade da sua empresa hoje, <b>seus clientes são Promotores ou Detratores?</b>
+						</p>
+		</section>
 				<ul class="tagsFooter list-inline my-3">
 					<li class="list-inline-item"><a href="#">NPS</a></li>
 					<li class="list-inline-item"><a href="#">Promotores da marca</a></li>
