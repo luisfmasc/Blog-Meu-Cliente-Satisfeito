91c57afcd9a0e233fd18cfb8dacf838ae2e3011c
Ivan Flecha Ribeiro <ivanflecharibeiro@gmail.com>
2019-02-20 11:29:42 -0300

    FAVICON 2.0

---
+++ nps-fechando-o-loop.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta property="og:url" content="https://meuclientesatisfeito.com"/>
+	<meta property="og:type" content="post"/>
+	<meta property="og:title" content="5 Motivos para praticar o NPS nas empresas brasileiras | Meu cliente satisfeito"/> <!---->
+	<meta property="og:description" content="Você já deve ter ouvido falar sobre NPS. Mas, vale a pena praticar o NPS nas empresas brasileiras? A resposta é sim, nesse artigo vamos te dizer porquê!"/> <!---->
+	<meta property="og:image" content="https://meuclientesatisfeito.com/images/institucional.png"/> <!--tumb-->
+	<meta property="description" content="Vale a pena praticar o NPS nas empresas brasileiras? A resposta é sim, nesse artigo vamos te dizer porquê!"/>
+	<title>5 Motivos | Meu cliente satisfeito</title> <!---->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<link href="css/style.css" rel="stylesheet">
+	<?php  $menuAtivo = 'post';
+	  include('components/analytics.html'); ?>
+	<script>
+		gtag( 'event', 'page_view', {
+			'send_to': 'UA-134713675-1',
+		} );
+	</script>
+</head>
+<body>
+	<header>
+		<?php include('components/menu.php'); ?>
+	</header>
+
+	<main id="politica" class="container">
+		<section class="row">
+			<main class="col-md-8">
+				<h1 class="mt-5">5 Motivos Para Praticar o NPS Nas Empresas Brasileiras| Meu cliente satisfeito</h1> <!---->
+				<small>19 de fevereiro de 2019, Luan Ciolette, NPS</small><br>
+				<figure class="figure my-5">
+					<img src="images/post/nps-nas-empresas-brasileiras-800x600.jpg" class="figure-img img-fluid rounded" alt="#PraCegoVer: [Fotografia] Três mãos de pessoas diferentes se cumprimentam. Ao fundo está uma mesa com alguns cadernos e um celular."> <!---->
+					<figcaption class="figure-caption">#PraCegoVer: [Fotografia] Três mãos de pessoas diferentes se cumprimentam. Ao fundo está uma mesa com alguns cadernos e um celular.</figcaption>
+				</figure>
+				<p> Você já deve ter ouvido falar sobre, já deve ter recebido uma recomendação e até já deve ter recebido uma pesquisa de NPS. Mas afinal, <b>vale a pena praticar o NPS nas empresa brasileiras? </b></p>
+				<p> Neste artigo, busco te convencer que sim e vou te explicar com dados por que as marcas que utilizam o Net Promoter Score saem na frente da concorrência em todos os sentidos.</p>
+				<h2>1. As grandes marcas estão usando o NPS!</h2>
+				<p><b>O Net Promoter Score</b> é uma ferramenta excelente para avaliar a lealdade e a satisfação dos clientes, principalmente pela facilidade de compreensão e por ofertar resultados com alto nível de confiabilidade, claro, se aplicada corretamente.</p>
+				<p>A oportunidade de aprimorar a experiência do cliente em algo positivo e marcante, tem feito do NPS um dos principais motivos para gestores utilizarem a ferramenta como métrica de performance do Customer Experience, além da métrica estar diretamente relacionada aos lucros e resultados das companhias.</p>
+				<p>Abaixo listamos algumas das principais empresas no mundo que usam o Net Promoter Score para mensurar o grau de <b>lealdade e satisfação de seus clientes</b> e assim aperfeiçoar seus produtos e serviços aumentando seus lucros:</p>
+				<figure class="figure my-5">
+					<img src="images/post/logotipo-empresas-nps.jpg" class="figure-img img-fluid rounded" alt="Logos de empresas usando NPS">
+					<figcaption class="figure-caption">#PraCegoVer: [Gráfico] Com diversos logos de empresas usando NPS.</figcaption>
+				</figure>
+				<h2>2. O NPS é importante para o sucesso da sua empresa!</h2>
+				<p>Diversas empresas utilizam esta metodologia para medir a satisfação de seus clientes e verificar em quais áreas é necessário um plano de ação. O que Allianz Group, Canon, Ebay, Facebook, HP, Microsoft e General Motors tem em comum? Todas utilizam esta metodologia em suas empresas!</p>
+				<p>O NPS hoje é a principal métrica focada na fidelidade e experiência do cliente no mundo, ou seja, impacta diretamente no <strong>resultado e na percepção do cliente</strong> ao adquirir produtos e serviços dessa marca. Influenciando ainda na recomendação de fato, ou seja, fazem com que os consumidores recomendem a marca a amigos e familiares.</p>
+				<p>A aplicação NPS está tão difundida, que atualmente <a href="https://customer.guru/net-promoter-score/fortune-500" target="_blank" rel="noopener"><strong>95% das 500 maiores empresas</strong></a> no mundo utilizam essa métrica de lealdade como termômetro de relacionamento com os clientes.</p>
+				<h2>3. Sua empresa deve utilizar o NPS!</h2>
+				<p>A Bain Company produziu um <a href="https://www.bain.com/insights/introducing-the-net-promoter-system-loyalty-insights/" target="_blank" rel="noopener">estudo focado em NPS</a> de instituições financeiras no Brasil e mostrou que apesar do crescimento orgânico em suas receitas, os bancos de média renda possuem um baixo NPS, ou seja, maior volume de clientes detratores. Os bancos de alta renda foram as instituições que apresentaram maior NPS em relação aos de média renda.</p>
+				<p>Nestes casos, quanto maior o número de clientes promotores, maior a aceitação de produtos e a permanência dos clientes com a empresa. Uma vigilância contínua desta métrica possibilita uma revisão constante dos pontos que atraem ou que repelem os clientes, gerando assim oportunidades de melhorar estes serviços para conquistar um número maior de promotores, concomitantemente aumento na retenção e receitas das empresas.</p>
+				<h2>4. As vantagens do NPS</h2>
+				<p>Vamos avaliar os aspectos positivos do sistema NPS e ver como sua empresa se beneficia se os resultados forem aplicados às suas práticas de negócios.</p>
+				<h3>Intuitivo e fácil de usar</h3>
+				<p>A pergunta do modelo de questionário se baseia em uma ideia: descobrir se os seus <strong>clientes gostam o bastante da sua empresa a ponto de recomendá-la</strong>. Esta pergunta costuma ser acompanhada por outra a fim de descobrir porque as pessoas recomendam ou não. Você pode facilmente enviar o questionário para os clientes por e-mail, whatsapp ou até publicá-lo no seu site. O cálculo é intuitivo e exige apenas uma <b>planilha</b>  básica para funcionar.</p>
+				<h3>Excepcional para gestão</h3>
+				<p>Quando os executivos da sua empresa precisam de uma medida prática, rápida e abrangente da fidelidade dos seus clientes, o NPS é uma ótima solução. Além de ajudar a empresa a <strong>comparar seu desempenho ao da concorrência</strong> e pode ser usado pelos gestores para comparar seus diversos produtos ou serviços.</p>
+				<h3>Padroniza a classificação da clientela</h3>
+				<p>A metodologia NPS se subdivide em três categorias de clientes: <strong>Promotores, Detratores e Passivos</strong>. Estas categorias facilitam a classificação da fidelidade dos clientes e padroniza a comunicação e os esforços da sua empresa na hora de tratar sobre a clientela. </p>
+				<h3>Maior facilidade para se referenciar</h3>
+				<p>Um recurso essencial pois a medida é usada como padrão por empresas no mundo todo, facilitando a comparação do seu Net Promoter Score com os seus principais concorrentes. Sua pontuação se torna mais significativa em contexto com outras pontuações do seu setor.</p>
+				<h3>Não deixe o NPS para manhã!</h3>
+				<p>Seja qual for o segmento, nada que superasse o NPS foi inventado até os tempos atuais. Uma métrica que traz insights gerais sobre a<strong> experiência do cliente</strong>, trazendo uma métrica da lealdade que reflete no <strong>crescimento do faturamento</strong> das empresas. </p>
+				<p>Embora o NPS tenha seus fãs e críticos, a coisa mais importante a lembrar é que os clientes têm mais poder do que nunca para expressar suas opiniões, que podem criar ou danificar sua marca.</p>
+				<p>O mais importante é lembrar que o NPS irá gerar dados muito objetivos e de fácil comparação, perfeitos para acompanhamento e por isso, deve ser utilizado continuamente.</p>
+				<p>Para encerrar, o NPS é um ótimo ponto de partida justamente por sua simplicidade. Após a análise dos resultados, o trabalho real é agir sobre os feedbacks coletados, para começar a melhorar sua organização e desenvolver as ações necessárias para aumentar o NPS e permite que você avalie perfeitamente em qual etapa do processo de crescimento a sua organização está.</p>
+				<ul class="tagsFooter list-inline my-3">
+					<li class="list-inline-item"><a href="#">NPS</a></li>
+					<li class="list-inline-item"><a href="#">NPS no Brasil</a></li>
+					<li class="list-inline-item"><a href="#">Satisfação de clientes</a></li>
+					<li class="list-inline-item"><a href="#">Sucesso da marca</a></li>
+				</ul>
+			</main>
+			<aside class="col-md-3 offset-md-1">
+				<?php include('components/sidebar.php'); ?>
+			</aside>
+		</section>
+	</main>
+	<?php include('components/footer.php'); ?>
+</body>
+</html>
