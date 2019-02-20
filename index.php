7fd2fe0256b03fff2ed7769ef1615b4cafb8e53c
Luis Mascarenhas <contato@luisfmasc.com.br>
2019-02-20 13:41:06 -0300

    Arruma home

--- index.php
+++ index.php
@@ -40,9 +40,9 @@
         </div>
       </div>
       <div class="carousel-item carousel-item-next carousel-item-left">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Second slide"><title>Placeholder</title><rect fill="#666" width="100%" height="100%"></rect><text fill="#444" dy=".3em" x="50%" y="50%">Second slide</text></svg>
+        <img src="images/banner/nps-o-que-e-1900x400.jpg" />
         <div class="carousel-caption d-none d-md-block">
-          <h5>Second slide label</h5>
+          <h5>Descubra o poder do NPS</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
       </div>
@@ -65,27 +65,20 @@
   </div>
 	<!-- Fim do carousel-->
 
-	
 	<!-- Inicio do Destaque-->
 	<section id="destaqueHome" class="container my-4">
 		<section class="row">
 					<div class="list-inline-item col-4 mx-0">
-						<svg class="bd-placeholder-img mr-1 rounded-circle" width="110" height="110" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder:110x110">
-							<rect fill="#868e96" width="100%" height="100%"></rect><text fill="#dee2e6" dy="0.15em" x="20%" y="50%">110x110</text>
-						</svg>
-						<small>texto curto sobre nps, texto curto sobre nps, texto curto sobre nps</small>
+						<img src="images/post/destaque-home-01.jpg" class="rounded-circle" width="110" height="110" alt="Balão de cor com a letra N, na sigla, que significa Net"/>
+						<small>Sua rede de clientes</small>
 					</div>
 					<div class="list-inline-item col-4 mx-0">
-						<svg class="bd-placeholder-img mr-1 rounded-circle" width="110" height="110" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder:110x110">
-							<rect fill="#868e96" width="100%" height="100%"></rect><text fill="#dee2e6" dy="0.15em" x="20%" y="50%">110x110</text>
-						</svg>
-						<small>texto curto sobre nps, texto curto sobre nps, texto curto sobre nps</small>
+						<img src="images/post/destaque-home-02.jpg" class="rounded-circle" width="110" height="110" alt="Balão de cor com a letra P, na sigla, que significa Promoter"/>
+						<small>Promove seu negócio?</small>
 					</div>
 					<div class="list-inline-item col-4 mx-0">
-						<svg class="bd-placeholder-img mr-1 rounded-circle" width="110" height="110" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder:110x110">
-							<rect fill="#868e96" width="100%" height="100%"></rect><text fill="#dee2e6" dy="0.15em" x="20%" y="50%">110x110</text>
-						</svg>
-						<small>texto curto sobre nps, texto curto sobre nps, texto curto sobre nps</small>
+						<img src="images/post/destaque-home-03.jpg" class="rounded-circle" width="110" height="110" alt="Balão de cor com a letra S, na sigla, que significa Score" />
+						<small>Descubra sua pontuação!</small>
 					</div>
 			</section>
 	</section>	
@@ -95,8 +88,34 @@
 	<section class="container">
 		<section class="row">
 			<main class="col-md-8">
-				
-				
+                <div class="row">
+                    <div class="row postHome">                        
+                        <figure class="col-3">
+                            <img src="images/post/nps-o-que-e-800x600.jpg" width="100%" class="img-thumbnail" />
+                        </figure>
+                        <div class="col-9">
+                            <h3>NPS: O que é e porque é importante para sua empresa</h3>
+                            <p>Você sabe o quanto seu cliente está satisfeito com a empresa? Descubra o que é Net Promoter Score (NPS) e saiba porque é importante para o crescimento da empresa.</p>
+                        </div>
+                    </div>
+                    <div class="row postHome">                        
+                        <figure class="col-3">
+                            <img src="images/thumbs/o-basico-de-nps-100x100.jpg" width="100%" class="img-thumbnail" />
+                        </figure>
+                        <div class="col-9">
+                            <h3>3 fatos sobre NPS que você precisa saber</h3>
+                            <p>Conheça #3 fatos sobre NPS que te ajudarão a entender melhor esta tecnologia. Como surgiu, como medir e com qual frequência pode ser aplicado.</p>
+                        </div>
+                    </div>
+                    <div class="row postHome">                        
+                        <figure class="col-3">
+                            <img src="images/thumbs/nps-nas-empresas-brasileiras-100x100.jpg" width="100%" class="img-thumbnail" />
+                        </figure>
+                        <div class="col-9">
+                            <h3>5 Motivos para praticar o NPS nas empresas brasileiras</h2>
+                            <p>Você já deve ter ouvido falar sobre NPS. Mas, vale a pena praticar o NPS nas empresas brasileiras? A resposta é sim, nesse artigo vamos te dizer porquê!</p>
+                        </div>
+				    </div>
 			</main>
 			<aside class="col-md-3 offset-md-1">
 				<?php include('components/sidebar.php'); ?>
