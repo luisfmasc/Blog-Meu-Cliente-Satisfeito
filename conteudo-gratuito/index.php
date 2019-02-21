16e30fa03a16aa727fd2e2977404d50c696de029
Luis Mascarenhas <contato@luisfmasc.com.br>
2019-02-21 15:17:07 -0300

    Incorpora o footer

--- conteudo-gratuito/index.php
+++ conteudo-gratuito/index.php
@@ -91,7 +91,26 @@
         </div>
       </div>
     </div>
-	<?php include ('../components/footerCompacto.php') ?> 
-  </body>
+<footer class="bgAzulEscuro">
+	<div class="container">
+		<div class="row">
+			<div class="col center" id="endereco" onClick="gtag('event', 'endereco', {'event_category' : 'footer', 'event_label' : 'selecionaEndereco'})">
+				<a class="AzulClaro" href="mailto:contato@meuclientesatisfeito.com">contato@meuclientesatisfeito.com</a>
+			</div>
+		</div>
+	</div>
+</footer>
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.min.css" integrity="sha256-rFMLRbqAytD9ic/37Rnzr2Ycy/RlpxE5QH52h7VoIZo=" crossorigin="anonymous" />
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    
+	<script src="../js/docs.js"></script>   
+
+
+<!--Firebase assync-->
+<script src="https://www.gstatic.com/firebasejs/5.8.3/firebase.js"></script>
+<script src="../js/main.js"></script>  </body>
 
 </html>
