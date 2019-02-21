55cc11343c60c8948d515a70c89b7cbad3df8ab6
Luis Mascarenhas <contato@luisfmasc.com.br>
2019-02-21 02:52:12 -0300

    Criar Landing Page

---
+++ landing-page/index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta property="og:url" content="https://meuclientesatisfeito.com/landing-page"/>
+        <meta property="og:type" content="product"/>
+        <meta property="og:title" content="Meu cliente satisfeito"/>
+        <!--		<meta property="og:description"        content="" />-->
+        <meta property="og:image" content="https://meuclientesatisfeito.com/images/institucional.png"/>
+        <!--		<meta name="description" content=""/>
+            <meta name="robots" content="index,follow"/>-->
+        <meta property="og:locale" content="pt_BR"/>
+        <title>Meu cliente satisfeito </title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link href="../css/stylePages.css" rel="stylesheet">
+        <?php include('../components//analytics.html'); ?>
+    </head>
+    <body>
+	<section id="bannerInstitucional">
+		<header class="container" >
+			<div id="logo" class="col-sm-6 col-sm-offset-3">
+				<img class="img-responsive" src="../images/logotipoClienteSatisfeitoNegativo.png" alt="Logotipo da Meu Cliente Satisfeito" height="100" />
+			</div>
+		</header>
+		<section class="container">
+			<div style="row">
+				<div class="col-md-6">
+						<h1 class="grande longo">Um Cliente satisfeito é sua melhor estratégia de negócios!</h1>
+					<div class="negativo">
+						<p>Esse material é exclusivo para você que mata um leão por dia no mundo dos negócios e quer que sua marca seja referência no seu segmento. Com esse material vamos te ensinar táticas simples, eficaz e muito usada pelos grandes players do mercado para fidelizar seus clientes e ganhar muito mais dinheiro com isso, mesmo se você está começando agora e até para as grandes empresas que querem vender mais.  Tudo isso com uma metodologia descomplicada de coleta do feedback do cliente. Não deixe de pegar seu conteúdo GRATUITO e fazer parte desse grupo seleto das empresas mais rentáveis do mundo.</p>
+                        <figure><img src="../images/logotipos.png" alt="Gráfico com logotipos de empresas que usan NPS" height="40" width="250" /></figure>
+                        <button class="azulClaro">Quero participar desse grupo agora</button>
+					</div>
+				</div>
+				<div id="form" class="col-md-5 col-md-offset-1" >
+					<figure><img src="../images/mockup.png" alt="Gráfico ilustrando o e-book" width="100%" /></figure>
+				</div>
+			</div>
+		</section>
+		<section class="container">
+			<div id="content" class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <h2>O conteúdo</h2>
+                    <p>Resolvi criar esse conteúdo para que juntos possamos descobrir o quanto os clientes estão satisfeitos com sua marca, aplicando uma metodologia rápida, prática e altamente efetiva para fidelizar seu cliente, e nada melhor que um cliente fiel, que além de comprar com você vai recomendar sua marca para outras pessoas sem que você gaste 1 real a mais, isso mesmo Custo de Aquisição 0 (zero!) para mais clientes qualificados e com intenção clara de aquisição!</p>
+                    <p>Mas não posso fazer isso sozinho, preciso que você se comprometa a ler todo o material, usar a planilha, exercitar na prática e caso tenha alguma dúvida nós da <a href="https://meuclientesatisfeito.com">Meu Cliente Satisfeito</a> estamos aqui para te ajudar, é só nos enviar um e-mail com as dúvidas!</p>                  
+                    <h3>O que você vai encontrar no material:</h3>
+                    <ol>
+                        <li>Como satisfazer seu cliente</li>
+                        <li>Como aprender com seu cliente</li>
+                        <li>Como fidelizar seu cliente</li>
+                        <li>Como implantar e extrair os resultados do NPS.</li>
+                        <li>Como agir para melhorar seus resultados.</li>
+                        <li>Uma Planilha pronta para você começar a executar.</li>
+                    </ol>
+                    <button class="azulEscuro">Ok, você me convenceu, me dá logo esse material!</button>
+                </div>
+
+			</div>
+		</section>
+	</section>	  
+
+	<?php include ('../components/footerCompacto.php') ?> 
+  </body>
+
+</html>
