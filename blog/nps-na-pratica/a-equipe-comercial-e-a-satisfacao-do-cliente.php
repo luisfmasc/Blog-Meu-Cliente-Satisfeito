4eadf4dc792abe4ac85ecf4965de21b14a8de902
Ivan Flecha Ribeiro <ivanflecharibeiro@gmail.com>
2019-02-21 19:58:34 -0300

    Upado o post #10
    https://docs.google.com/document/d/1FpvsfR1B3VP0lLvPG6RVFVUnqr_4SDFAnk4XHqaq1XY/edit

---
+++ blog/nps-na-pratica/a-equipe-comercial-e-a-satisfacao-do-cliente.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta property="og:url" content="https://meuclientesatisfeito.com"/>
+	<meta property="og:type" content="post"/>
+	<meta property="og:title" content="5 Motivos para praticar o NPS nas empresas brasileiras | Meu cliente satisfeito"/> <!---->
+	<meta property="og:description" content="Você já deve ter ouvido falar sobre NPS. Mas, vale a pena praticar o NPS nas empresas brasileiras? A resposta é sim, nesse artigo vamos te dizer porquê!"/> <!---->
+	<meta property="og:image" content="https://meuclientesatisfeito.com/images/institucional.png"/> <!--tumb-->
+	<meta property="description" content="Vale a pena praticar o NPS nas empresas brasileiras? A resposta é sim, nesse artigo vamos te dizer porquê!"/>
+	<title>5 Motivos | Meu cliente satisfeito</title> <!---->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<link href="'../../css/style.css" rel="stylesheet">
+	<?php  $menuAtivo = 'post';
+	  include('../../components/analytics.html'); ?>
+</head>
+<body>
+	<header>
+		<?php include('../../components/menu.php'); ?>
+	</header>
+
+	<main id="politica" class="container">
+		<section class="row">
+			<main class="col-md-8">
+				<h1 class="mt-5">5 Motivos Para Praticar o NPS Nas Empresas Brasileiras| Meu cliente satisfeito</h1> <!---->
+				<small>19 de fevereiro de 2019, Luan Ciolette, NPS</small><br>
+				<figure class="figure my-5">
+					<img src="../../images/post/especializar a equipe comercial" class="figure-img img-fluid rounded" alt="#PraCegoVer: [Fotografia] Duas pessoas num aperto de mãos e uma chave sendo entregue em gesto de confiança"> <!---->
+					<figcaption class="figure-caption">#PraCegoVer: [Fotografia] Duas pessoas num aperto de mãos e uma chave sendo entregue em gesto de confiança</figcaption>
+				</figure>
+				<h1><p> A equipe comercial e a satisfação do cliente - como treinar seu time para melhorar o NPS</p></h1>
+				<p> Autoridades do ramo chamam-na de “o maior multiplicador de vendas” e, mesmo assim, grande parte das empresas brasileiras não adotam a <strong>especialização da equipe comercial</strong> como prioridade. Mas aqui vai uma dica preciosa: seu <strong>time de vendas está diretamente ligado à satisfação do cliente</strong>!</p>
+				<p> Pense na quantidade de engajamento que o time comercial precisa ter com a empresa e com o cliente para alcançar resultados e o quanto isso pode ajudar ou prejudicar a empresa.</p>
+				<h2><b>A equipe comercial e a satisfação do cliente</b></h2>
+				<p> Vamos exemplificar:</p>
+				<p> Um time comercial qualificado é capaz de:</p>
+				<ul>
+					<li> conquistar o cliente nos primeiros contatos, aumentando as chances de conversão</li>
+					<li>saber o momento certo de fechar a venda, para não perder e nem antecipar a decisão do cliente</li>
+					<li> iniciar um relacionamento cliente x empresa, sendo a “primeira impressão” que o cliente tem</li>
+					<li> criar vínculo e iniciar a fidelização do cliente, melhorando o Lifetime Value</li>
+					<li> buscar indicações com o cliente, aumentando a carteira</li>
+					<li> identificar o perfil do cliente e oferecer produtos/serviços que possam agradá-lo</li>
+					<li> ser organizado, cumprir com sua agenda corretamente e não deixar o cliente esperando</li>
+				</ul>
+				</br>
+				<p> Agora, veja o impacto da equipe comercial sem qualificação:</p>
+				<ul>
+					<li> não consegue abordar o cliente de forma correta, passando uma imagem ruim da empresa</li>
+					<li> não sabe o momento certo de fechar a venda, assustando o cliente e por vezes, perdendo-o</li>
+					<li> não se preocupa em estabelecer um relacionamento cliente x empresa, pensa somente naquela venda</li>
+					<li> não se preocupa com a fidelização do cliente</li>
+					<li> não estuda o perfil do cliente, oferecendo produtos e serviços que podem não ser coerentes com seus interesses</li>
+					<li> vende somente o que o cliente pediu, sem oferecer opções</li>
+					<li> não organiza e cumpre com sua agenda, combina algo com o cliente e não cumpre</li>
+				</ul>
+				<p> Estes são alguns dos impactos sobre a qualificação do time. Agora, o que isso tudo tem a ver com NPS? Simples: </p>
+				<p> style="color: #1c3997;"><strong>Cliente bem atendido = cliente feliz</strong></p>
+				<p> style="color: #1c3997;"><strong>Cliente feliz = NPS alto</strong></p>
+				<p> Uma empresa que almeja o sucesso e deseja crescer em escalas cada vez maiores precisa colocar em prática a especialização do time comercial, baseando-se no seguinte modelo:</p>
+				<p> <strong>1) INbound -&gt;</strong> É a parte da equipe que ficará responsável pela prospecção passiva de clientes: ela deverá contatar aqueles clientes potenciais que vêm até a empresa, de livre e espontânea vontade, sem serem procurados. Eles podem vir por meio de cadastros no site, indicações boca-a-boca, artigos do blog da empresa, entre outros meios. </p>
+				<blockquote><p> style="color: #1c3997;">Este time precisa ser: objetivo, esclarecedor, fidelizador, empático e engajador</p></blockquote>
+				<p> <strong>2) OUTbound -&gt;</strong> Trata-se da parcela da equipe que ficará encarregada da prospecção ativa de clientes. Sua atividade será ligar e mandar e-mails para compradores potenciais que não solicitaram e nunca entraram em contato com a empresa. As ligações e e-mails servirão para analisar se as instituições escolhidas precisam do seu produto ou serviço. </p>
+				<blockquote><p> style="color: #1c3997;">Este time precisa: ter simpatia, dominar o assunto, conquistar a atenção do cliente nas primeiras palavras</p></blockquote>
+				<p> <strong>3) Novos negócios -&gt;</strong> é a equipe de executivos de vendas para quem as primeiras duas encaminharão os clientes potenciais mais condizentes com o perfil da empresa e que, portanto, apresentam maiores chances de compra de produtos ou serviços. </p>
+				<blockquote><p> style="font color: #1c3997;">Pré-requisitos deste time: entender o perfil do cliente e oferecer outros produtos e serviços coerentes, identificar a dor do cliente e oferecer a melhor solução, conquistar a fidelidade, garantir que o cliente entendeu todos os pontos da compra, conseguir indicações.</p></blockquote>
+				<p> <strong>4) Pós-venda -&gt;</strong> refere-se ao time que fará a administração das novas contas ou a gestão do sucesso do cliente, após os executivos da terceira equipe efetivarem as vendas. Sua responsabilidade será prestar atendimento e suporte técnico, além de tomar medidas para reter os novos clientes e realizar futuras transações com eles. </p>
+				<blockquote><p> style="font-color: #1c3997;">Este time precisa: ter agilidade na resolução de problemas, dominar o assunto, manter o cliente ativo na empresa e cuidar para que todas as etapas da compra sejam concluídas com sucesso</p></blockquote>
+				<p> Desta forma, os clientes potenciais terão <strong>atendimento adequado</strong> de acordo com sua etapa no funil e com suas necessidades. A empresa precisa garantir a qualidade de todas as etapas da venda para que este cliente não seja um possível churn.</p>
+				<h3><p> Como medir a qualidade da equipe comercial e a satisfação dos clientes?</p></h3>
+				<p> Adote <strong>métricas</strong> para garantir que sua equipe esteja engajada e mantendo o padrão de atendimento. </p>
+				<p> Medir a qualidade da equipe comercial e a satisfação dos clientes é uma métrica única. Você pode fazer isso através da metodologia de pesquisa de satisfação chamada NPS, que significa “Net Promoter Score”.</p>
+				<p> O NPS tem uma pergunta principal: <p> style="color: #1c3997;"><strong>“Numa escolha de 0 a 10, qual a probabilidade do seu cliente recomendar sua empresa?”.</strong></p></p>
+				<p> Além desta, você pode incluir perguntas para medir a qualidade do atendimento da equipe comercial, como:</p>
+				<p> “De 0 a 10, o quão satisfeito você está com o atendimento prestado pelo time de vendas?”</p>
+				<p> Além disso, métricas já mais conhecidas podem te dizer muito sobre a qualidade do time e a satisfação do seu cliente, como índice de reclamações, churn e inadimplência. </p>
+				<p> Viu porque a equipe comercial é peça importante na melhoria da satisfação dos clientes?</p>
+				<ul class="tagsFooter list-inline my-3">
+					<li class="list-inline-item"><a href="#">NPS</a></li>
+					<li class="list-inline-item"><a href="#">NPS no Brasil</a></li>
+					<li class="list-inline-item"><a href="#">Satisfação de clientes</a></li>
+					<li class="list-inline-item"><a href="#">Sucesso da marca</a></li>
+				</ul>
+			</main>
+			<aside class="col-md-3 offset-md-1">
+				<?php include('../../components/sidebar.php'); ?>
+			</aside>
+		</section>
+	</main>
+	<?php include('../../components/footer.php'); ?>
+</body>
+</html>
