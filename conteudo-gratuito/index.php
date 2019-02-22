f5360323ffb0f3f0645bb198484e589723e08f84
Luis Mascarenhas <contato@luisfmasc.com.br>
2019-02-22 12:36:28 -0300

    Update index.php
    Testes com ivan

--- conteudo-gratuito/index.php
+++ conteudo-gratuito/index.php
@@ -88,10 +88,28 @@
                     <div class="sucesso" style="display: none">Cadastro realizado com sucesso!</div>
                 </form>
             </section>
-            </div>
+            </div>            
         </div>
       </div>
     </div>
+        
+<div class="modal fade" id="modalOk" tabindex="-1" role="dialog" aria-labelledby="modalOkTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div id="FormOk" class="modal-content no-header">
+        <div class="close">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+        </button>
+        </div>
+      <div class="modal-body">
+        <section class="p-5">
+            <h1>Obrigado ;)</h1>
+            <p>Seu cadastro foi realizado com sucesso!</p>
+        </section>
+        </div>
+    </div>
+  </div>
+</div>
 <footer class="bgAzulEscuro">
 	<div class="container">
 		<div class="row">
@@ -104,7 +122,16 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-
+<script>
+    $(function(){
+        $('#modalForm').submit(function(){
+            $('#modalForm').on('hidden.bs.modal'function (e) {
+                $('#modalOk').modal('show');
+                  })
+            return false;
+        }); 
+    });
+</script>
     <!--Firebase -->
     <script src="https://www.gstatic.com/firebasejs/5.8.3/firebase.js"></script>
     <script src="https://meuclientesatisfeito.com/js/main.js"></script>  </body>
