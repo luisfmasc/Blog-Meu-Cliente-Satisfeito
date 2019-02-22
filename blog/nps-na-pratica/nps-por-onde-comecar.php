cdab10202e4c63410b0ec57b35e021614870b469
Ivan Flecha Ribeiro <ivanflecharibeiro@gmail.com>
2019-02-22 19:35:17 -0300

    Criei post e removi o htaccess

---
+++ blog/nps-na-pratica/nps-por-onde-comecar.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta property="og:url" content="https://meuclientesatisfeito.com"/>
+	<meta property="og:type" content="post"/>
+	<meta property="og:title" content="5 Motivos para praticar o NPS nas empresas brasileiras | Meu cliente satisfeito"/> <!---->
+	<meta property="og:description" content="Você já deve ter ouvido falar sobre NPS. Mas, vale a pena praticar o NPS nas empresas brasileiras? A resposta é sim, nesse artigo vamos te dizer porquê!"/> <!---->
+	<meta property="og:image" content="https://meuclientesatisfeito.com/images/institucional.png"/> <!--tumb-->
+	<meta property="description" content="Vale a pena praticar o NPS nas empresas brasileiras? A resposta é sim, nesse artigo vamos te dizer porquê!"/>
+	<title>5 Motivos | Meu cliente satisfeito</title> <!---->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">	<link href="https://meuclientesatisfeito.com/css/style.css" rel="stylesheet">
+	<?php  $menuAtivo = 'post';
+	  include('https://meuclientesatisfeito.com/components/analytics.html'); ?>
+</head>
+<body>
+	<header>
+		<?php include('https://meuclientesatisfeito.com/components/menu.php'); ?>
+	</header>
+
+	<main id="politica" class="container">
+		<section class="row">
+			<main class="col-md-8">
+				<h1 class="mt-5">NPS: Por onde começar?| Meu cliente satisfeito</h1> <!---->
+				<small>19 de fevereiro de 2019, Thamires Rocha, NPS</small><br>
+				<figure class="figure my-5">
+					<img src="https://meuclientesatisfeito.com/images/post/nps-nas-empresas-brasileiras-800x600.jpg" class="figure-img img-fluid rounded" alt="#PraCegoVer: [Fotografia] Três mãos de pessoas diferentes se cumprimentam. Ao fundo está uma mesa com alguns cadernos e um celular."> <!---->
+					<figcaption class="figure-caption">#PraCegoVer: [Fotografia] Três mãos de pessoas diferentes se cumprimentam. Ao fundo está uma mesa com alguns cadernos e um celular.</figcaption>
+				</figure>
+				<p> Quer <strong>aplicar o</strong> <strong>Net Promoter Score</strong> na sua empresa e não sabe por onde começar? Nesse post explico os cinco primeiros passos para implementar o NPS.</p>
+				<h3>1- Defina seu público</h3>
+				<p> Quem receberá a pesquisa? Toda a sua base ativa ou os clientes que geraram churn? Defina seu público pensando nas ações que tomará no futuro após o resultado da pesquisa.</p>
+				<h3>2- Onde a pesquisa será aplicada?</h3>
+				<p> Pense em qual canal de comunicação você utilizará, e-mail, SMS ou até mesmo através de uma Landing Page, pense de acordo com seu público  qual canal irá gerar mais engajamento e aderência.</p>
+				<h3>3- Adapte as perguntas ao seu negócio</h3>
+				<p> De acordo com a realidade da sua empresa faça as adaptações na “pergunta definitiva”  como:</p>
+				<ul>
+					<li><p> o quão satisfeito você está</p></li>
+					<li><p> conte-nos como foi sua experiência</p></li>
+					<li>como podemos melhorar</li>
+					<li></li>
+				</ul>
+				<p> Não se esqueça da pergunta qualitativa, através dela que você terá insumos dos pontos a melhorar no seu processo e produto.</p>
+				<h3><b>4- Mão na massa</b></h3>
+				<p> Após definir os passos acima, envie a pesquisa, é importante que o cliente reconheça sua empresa então identifique-se por meio da sua marca.</p>
+				<p> Faça testes antes do envio e certifique-se que que as respostas estão sendo armazenadas de forma correta.</p>
+				<p> Acompanhe a taxa de resposta, caso a aderência esteja baixa, considere enviar um follow-up.</p>
+				<p> Caso você queira elaborar a pesquisa e coletar as respostas de maneira mais profissional sem precisar desenvolver uma plataforma própria, pode contar empresas que são referência: <a href="https://www.opinionbox.com/"> Opinion Box</a>,<a href="https://opinia.me/"> Opinia</a> e a <a href="https://tracksale.co/"> Tracksale.</a></p>
+				<b>5- Hora da verdade</b>
+				<p> Com resultados em mãos, identifique os perfis dos clientes </p><b>detratores, neutros e promotores</b><p> , defina planos de ação, foco nos clientes detratores e nas ações de melhorias.</p>
+				<p> Segmente os resultados de acordo sua necessidade, além do NPS global da sua empresa, você pode segmentar por região geográfica, por exemplo, e trabalhar com campanhas de marketing mais direcionadas.</p>
+			</main>
+			<aside class="col-md-3 offset-md-1">
+				<?php include('https://meuclientesatisfeito.com/components/sidebar.php'); ?>
+			</aside>
+		</section>
+	</main>
+	<?php include('https://meuclientesatisfeito.com/components/footer.php'); ?>
+</body>
+</html>
